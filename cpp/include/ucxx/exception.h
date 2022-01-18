cefb53d242c34ff213d3f4147fe1720382853d18
Peter Andreas Entschev <peter@entschev.com>
2022-01-18 08:39:06 -0800

    Add UCXXError/UCXXConfigError exceptions

---
+++ cpp/include/ucxx/exception.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+ *
+ * See file LICENSE for terms.
+ */
+#pragma once
+
+#include <exception>
+#include <string>
+
+namespace ucxx
+{
+
+
+class UCXXError : public std::exception
+{
+    private:
+        std::string _msg{};
+
+    public:
+        UCXXError(const std::string& msg) : _msg{msg}
+        {
+        }
+
+        virtual const char* what() const noexcept override
+        {
+            return this->_msg.c_str();
+        }
+};
+
+class UCXXConfigError : public UCXXError
+{
+    public:
+        UCXXConfigError(const std::string& msg) : UCXXError(msg)
+        {
+        }
+};
+
+
+} // namespace ucxx
