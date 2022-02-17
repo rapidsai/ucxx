99c4e4eadb6ab3765837abd880f44519aaac23d0
Peter Andreas Entschev <peter@entschev.com>
2022-02-17 13:48:56 -0800

    Add wireup message in C++ basic example

--- cpp/examples/basic.cpp
+++ cpp/examples/basic.cpp
@@ -137,50 +137,8 @@ ucs_status_t parseCommand(int argc, char* const argv[])
     return UCS_OK;
 }
 
-int main(int argc, char **argv)
+void waitPromises(std::shared_ptr<ucxx::UCXXWorker> worker, std::vector<std::shared_ptr<ucxx::ucxx_request_t>>& promises)
 {
-    if (parseCommand(argc, argv) != UCS_OK)
-        return -1;
-
-    auto context = ucxx::UCXXContext::create({}, ucxx::UCXXContext::default_feature_flags);
-    auto worker = context->createWorker();
-    auto listener_ctx = std::make_unique<ListenerContext>(worker);
-    auto listener = worker->createListener(listener_port, listener_cb, listener_ctx.get());
-    listener_ctx->setListener(listener);
-    auto endpoint = worker->createEndpointFromHostname("127.0.0.1", listener_port, true);
-
-    if (progress_mode == PROGRESS_MODE_BLOCKING)
-        worker->init_blocking_progress_mode();
-    else
-        worker->startProgressThread();
-
-    // Block until client connects
-    while (listener_ctx->isAvailable())
-    {
-        if (progress_mode == PROGRESS_MODE_BLOCKING)
-            worker->progress_worker_event();
-        // Else progress thread is enabled
-    }
-
-    std::vector<std::shared_ptr<ucxx::ucxx_request_t>> promises;
-
-    std::vector<int> sendBuffer(5);
-    std::iota(std::begin(sendBuffer), std::end(sendBuffer), 0);
-    std::vector<int> sendBuffer2(10000);
-    std::iota(std::begin(sendBuffer2), std::end(sendBuffer2), 0);
-
-    size_t msgLength = sendBuffer.size() * sizeof(int);
-    size_t msgLength2 = sendBuffer2.size() * sizeof(int);
-
-    std::vector<int> recvBuffer(sendBuffer.size());
-    std::vector<int> recvBuffer2(sendBuffer2.size());
-
-    // Schedule send and recv messages on different tags and different orders.
-    promises.push_back(listener_ctx->getEndpoint()->tag_send(sendBuffer.data(), msgLength, 0));
-    promises.push_back(listener_ctx->getEndpoint()->tag_recv(recvBuffer2.data(), msgLength2, 1));
-    promises.push_back(endpoint->tag_send(sendBuffer2.data(), msgLength2, 1));
-    promises.push_back(endpoint->tag_recv(recvBuffer.data(), msgLength, 0));
-
     // Wait until all messages are completed
     if (progress_mode == PROGRESS_MODE_BLOCKING)
     {
@@ -200,12 +158,76 @@ int main(int argc, char **argv)
         for (auto& p : promises)
             assert_ucs_status(p->completed_promise.get_future().get());
     }
+}
 
-    for (size_t i = 0; i < sendBuffer.size(); i++)
-        assert(recvBuffer[i] == sendBuffer[i]);
+int main(int argc, char **argv)
+{
+    if (parseCommand(argc, argv) != UCS_OK)
+        return -1;
+
+    // Setup: create UCP context, worker, listener and client endpoint.
+    auto context = ucxx::UCXXContext::create({}, ucxx::UCXXContext::default_feature_flags);
+    auto worker = context->createWorker();
+    auto listener_ctx = std::make_unique<ListenerContext>(worker);
+    auto listener = worker->createListener(listener_port, listener_cb, listener_ctx.get());
+    listener_ctx->setListener(listener);
+    auto endpoint = worker->createEndpointFromHostname("127.0.0.1", listener_port, true);
+
+    // Initialize worker progress
+    if (progress_mode == PROGRESS_MODE_BLOCKING)
+        worker->init_blocking_progress_mode();
+    else
+        worker->startProgressThread();
+
+    // Block until client connects
+    while (listener_ctx->isAvailable())
+    {
+        if (progress_mode == PROGRESS_MODE_BLOCKING)
+            worker->progress_worker_event();
+        // Else progress thread is enabled
+    }
+
+    std::vector<std::shared_ptr<ucxx::ucxx_request_t>> promises;
+
+    // Allocate send buffers
+    std::vector<int> sendWireupBuffer{1, 2, 3};
+    std::vector<int> sendBuffer1(500);
+    std::iota(std::begin(sendBuffer1), std::end(sendBuffer1), 0);
+    std::vector<int> sendBuffer2(10000);
+    std::iota(std::begin(sendBuffer2), std::end(sendBuffer2), 0);
+
+    // Allocate receive buffers
+    std::vector<int> recvWireupBuffer(sendWireupBuffer.size(), 0);
+    std::vector<int> recvBuffer1(sendBuffer1.size(), 0);
+    std::vector<int> recvBuffer2(sendBuffer2.size(), 0);
+
+    // Compute message lengths
+    size_t wireupMsgLength = sendWireupBuffer.size() * sizeof(int);
+    size_t msgLength1 = sendBuffer1.size() * sizeof(int);
+    size_t msgLength2 = sendBuffer2.size() * sizeof(int);
+
+    // Schedule small wireup messages to let UCX identify capabilities between endpoints
+    promises.push_back(listener_ctx->getEndpoint()->tag_send(sendWireupBuffer.data(), wireupMsgLength, 0));
+    promises.push_back(endpoint->tag_recv(recvWireupBuffer.data(), wireupMsgLength, 0));
+
+    // Schedule send and recv messages on different tags and different orders
+    promises.push_back(listener_ctx->getEndpoint()->tag_send(sendBuffer1.data(), msgLength1, 0));
+    promises.push_back(listener_ctx->getEndpoint()->tag_recv(recvBuffer2.data(), msgLength2, 1));
+    promises.push_back(endpoint->tag_send(sendBuffer2.data(), msgLength2, 1));
+    promises.push_back(endpoint->tag_recv(recvBuffer1.data(), msgLength1, 0));
+
+    // Wait for promises to be set, i.e., transfers complete
+    waitPromises(worker, promises);
+
+    // Verify results
+    for (size_t i = 0; i < sendWireupBuffer.size(); i++)
+        assert(recvWireupBuffer[i] == sendWireupBuffer[i]);
+    for (size_t i = 0; i < sendBuffer1.size(); i++)
+        assert(recvBuffer1[i] == sendBuffer1[i]);
     for (size_t i = 0; i < sendBuffer2.size(); i++)
         assert(recvBuffer2[i] == sendBuffer2[i]);
 
+    // Stop progress thread
     if (progress_mode == PROGRESS_MODE_THREADED)
         worker->stopProgressThread();
 
