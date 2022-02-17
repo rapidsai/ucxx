2744424347d9a3f3098a29877253ae1cb08b4dd3
Peter Andreas Entschev <peter@entschev.com>
2022-02-17 09:29:03 -0800

    Add CLI arguments and multiple progress modes to C++ basic example

--- cpp/examples/basic.cpp
+++ cpp/examples/basic.cpp
@@ -1,11 +1,19 @@
+#include <cassert>
+#include <chrono>
 #include <numeric>
 #include <thread>
 #include <vector>
-#include <cassert>
 
 #include <ucxx/api.h>
 #include <ucxx/sockaddr_utils.h>
 
+
+enum progress_mode_t {
+    PROGRESS_MODE_BLOCKING,
+    PROGRESS_MODE_THREADED
+} progress_mode = PROGRESS_MODE_BLOCKING;
+
+static uint16_t listener_port = 12345;
 
 class ListenerContext
 {
@@ -51,7 +59,6 @@ class ListenerContext
 
             static bool endpoint_error_handling = true;
             _endpoint = _listener->createEndpointFromConnRequest(conn_request, endpoint_error_handling);
-            std::cout << "Listener Endpoint: " << std::hex << _endpoint.get() << std::endl;
         }
 
         void releaseEndpoint()
@@ -86,22 +93,76 @@ static void listener_cb(ucp_conn_request_h conn_request, void *arg)
     }
 }
 
-int main()
+static void printUsage()
 {
+    std::cerr << "Usage: basic [parameters]" << std::endl;
+    std::cerr << "UCXX basic client/server example" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Parameters are:" << std::endl;
+    std::cerr << "  -b          Use blocking progress mode" << std::endl;
+    std::cerr << "  -t          Use threaded progress mode" << std::endl;
+    std::cerr << "  -p <port>   Port number to listen at" << std::endl;
+    std::cerr << "  -h          Print this help" << std::endl;
+    std::cerr << std::endl;
+}
+
+ucs_status_t parseCommand(int argc, char* const argv[])
+{
+    int c;
+    while ((c = getopt(argc, argv, "btp:")) != -1)
+    {
+        switch (c)
+        {
+            case 'b':
+                progress_mode = PROGRESS_MODE_BLOCKING;
+                break;
+            case 't':
+                progress_mode = PROGRESS_MODE_THREADED;
+                break;
+            case 'p':
+                listener_port = atoi(optarg);
+                if (listener_port <= 0)
+                {
+                    std::cerr << "Wrong listener port: " << listener_port << std::endl;
+                    return UCS_ERR_UNSUPPORTED;
+                }
+                break;
+            case 'h':
+            default:
+                printUsage();
+                return UCS_ERR_UNSUPPORTED;
+        }
+    }
+
+    return UCS_OK;
+}
+
+int main(int argc, char **argv)
+{
+    if (parseCommand(argc, argv) != UCS_OK)
+        return -1;
+
     auto context = ucxx::UCXXContext::create({}, ucxx::UCXXContext::default_feature_flags);
     auto worker = context->createWorker();
     auto listener_ctx = std::make_unique<ListenerContext>(worker);
-    auto listener = worker->createListener(12345, listener_cb, listener_ctx.get());
+    auto listener = worker->createListener(listener_port, listener_cb, listener_ctx.get());
     listener_ctx->setListener(listener);
-    auto endpoint = worker->createEndpointFromHostname("127.0.0.1", 12345, true);
+    auto endpoint = worker->createEndpointFromHostname("127.0.0.1", listener_port, true);
 
-    worker->startProgressThread();
+    if (progress_mode == PROGRESS_MODE_BLOCKING)
+        worker->init_blocking_progress_mode();
+    else
+        worker->startProgressThread();
 
+    // Block until client connects
     while (listener_ctx->isAvailable())
     {
-        // Block until a progress thread to progress the worker
+        if (progress_mode == PROGRESS_MODE_BLOCKING)
+            worker->progress_worker_event();
+        // Else progress thread is enabled
     }
 
+    std::vector<std::shared_ptr<ucxx::ucxx_request_t>> promises;
 
     std::vector<int> sendBuffer(5);
     std::iota(std::begin(sendBuffer), std::end(sendBuffer), 0);
@@ -114,22 +175,39 @@ int main()
     std::vector<int> recvBuffer(sendBuffer.size());
     std::vector<int> recvBuffer2(sendBuffer2.size());
 
-    std::vector<std::shared_ptr<ucxx::ucxx_request_t>> promises;
-
-    // Scheduler send and recv messages on different tags and different orders.
+    // Schedule send and recv messages on different tags and different orders.
     promises.push_back(listener_ctx->getEndpoint()->tag_send(sendBuffer.data(), msgLength, 0));
     promises.push_back(listener_ctx->getEndpoint()->tag_recv(recvBuffer2.data(), msgLength2, 1));
     promises.push_back(endpoint->tag_send(sendBuffer2.data(), msgLength2, 1));
     promises.push_back(endpoint->tag_recv(recvBuffer.data(), msgLength, 0));
 
     // Wait until all messages are completed
-    for (auto& p : promises)
-        assert_ucs_status(p->completed_promise.get_future().get());
+    if (progress_mode == PROGRESS_MODE_BLOCKING)
+    {
+        for (auto& p : promises)
+        {
+            auto fut = p->completed_promise.get_future();
+            std::future_status status;
+            do
+            {
+                worker->progress_worker_event();
+                status = fut.wait_for(std::chrono::nanoseconds::zero());
+            } while (status != std::future_status::ready);
+        }
+    }
+    else
+    {
+        for (auto& p : promises)
+            assert_ucs_status(p->completed_promise.get_future().get());
+    }
 
     for (size_t i = 0; i < sendBuffer.size(); i++)
         assert(recvBuffer[i] == sendBuffer[i]);
     for (size_t i = 0; i < sendBuffer2.size(); i++)
         assert(recvBuffer2[i] == sendBuffer2[i]);
 
-    worker->stopProgressThread();
+    if (progress_mode == PROGRESS_MODE_THREADED)
+        worker->stopProgressThread();
+
+    return 0;
 }
