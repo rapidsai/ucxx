ef9014276dfcd21bcca0c8d6d437a7c7d59c1817
Peter Andreas Entschev <peter@entschev.com>
2022-03-21 08:05:51 -0700

    Set default UCX configs during import ucxx

--- python/ucxx/__init__.py
+++ python/ucxx/__init__.py
@@ -3,16 +3,72 @@
 
 
 import logging
-
-from . import testing  # noqa
-from . import _version
-from ._lib import libucxx  # type: ignore
-from .core import *  # noqa
-from .utils import get_address, get_ucxpy_logger  # noqa
+import os
 
 logger = logging.getLogger("ucx")
+
+# Notice, if we have to update environment variables we need to do it
+# before importing UCX, which must happen also before the Cython code
+# import to prevent UCS unused variable warnings.
+if "UCX_MEMTYPE_CACHE" not in os.environ:
+    # See <https://github.com/openucx/ucx/wiki/NVIDIA-GPU-Support#known-issues>
+    logger.debug("Setting env UCX_MEMTYPE_CACHE=n, which is required by UCX")
+    os.environ["UCX_MEMTYPE_CACHE"] = "n"
+
+from . import testing  # noqa
+from ._lib import libucxx  # type: ignore
+from ._version import get_versions
+from .core import *  # noqa
+from .utils import get_address, get_ucxpy_logger  # noqa
 
 # Setup UCX-Py logger
 logger = get_ucxpy_logger()
 
-__version__ = _version.get_versions()["version"]
+if "UCX_RNDV_THRESH" not in os.environ:
+    logger.info("Setting UCX_RNDV_THRESH=8192")
+    os.environ["UCX_RNDV_THRESH"] = "8192"
+
+if "UCX_RNDV_SCHEME" not in os.environ:
+    logger.info("Setting UCX_RNDV_SCHEME=get_zcopy")
+    os.environ["UCX_RNDV_SCHEME"] = "get_zcopy"
+
+if "UCX_CUDA_COPY_MAX_REG_RATIO" not in os.environ and get_ucx_version() >= (1, 12, 0):
+    try:
+        import pynvml
+
+        pynvml.nvmlInit()
+        device_count = pynvml.nvmlDeviceGetCount()
+        large_bar1 = [False] * device_count
+
+        for dev_idx in range(device_count):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(dev_idx)
+            total_memory = pynvml.nvmlDeviceGetMemoryInfo(handle).total
+            bar1_total = pynvml.nvmlDeviceGetBAR1MemoryInfo(handle).bar1Total
+
+            if total_memory <= bar1_total:
+                large_bar1[dev_idx] = True
+
+        if all(large_bar1):
+            logger.info("Setting UCX_CUDA_COPY_MAX_REG_RATIO=1.0")
+            os.environ["UCX_CUDA_COPY_MAX_REG_RATIO"] = "1.0"
+    except (
+        ImportError,
+        pynvml.NVMLError_LibraryNotFound,
+        pynvml.NVMLError_DriverNotLoaded,
+        pynvml.NVMLError_Unknown,
+    ):
+        pass
+
+if "UCX_MAX_RNDV_RAILS" not in os.environ and get_ucx_version() >= (1, 12, 0):
+    logger.info("Setting UCX_MAX_RNDV_RAILS=1")
+    os.environ["UCX_MAX_RNDV_RAILS"] = "1"
+
+
+__version__ = get_versions()["version"]
+__ucx_version__ = "%d.%d.%d" % get_ucx_version()
+
+if get_ucx_version() < (1, 11, 1):
+    raise ImportError(
+        f"Support for UCX {__ucx_version__} has ended. Please upgrade to "
+        "1.11.1 or newer."
+    )
