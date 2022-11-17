1068fd3b93b0bb328c99662a42211aebc3968fd2
Peter Andreas Entschev <peter@entschev.com>
2022-11-17 13:46:09 -0800

    Add Python async probe, send/recv and shutdown tests

---
+++ python/tests_async/test_probe.py
@@ -0,0 +1,49 @@
+import asyncio
+
+import pytest
+
+import ucxx as ucp
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("transfer_api", ["am", "tag"])
+async def test_message_probe(transfer_api):
+    if transfer_api == "am":
+        pytest.skip("AM not implemented yet")
+
+    msg = bytearray(b"0" * 10)
+
+    async def server_node(ep):
+        # Wait for remote endpoint to close before probing the endpoint for
+        # in-transit message and receiving it.
+        while not ep.closed():
+            await asyncio.sleep(0)  # Yield task
+
+        if transfer_api == "am":
+            assert ep._ep.am_probe() is True
+            received = await ep.am_recv()
+        else:
+            assert ep._ctx.worker.tag_probe(ep._tags["msg_recv"]) is True
+            received = bytearray(10)
+            await ep.recv(received)
+        assert received == msg
+
+    async def client_node(port):
+        ep = await ucp.create_endpoint(
+            ucp.get_address(),
+            port,
+        )
+        if transfer_api == "am":
+            await ep.am_send(msg)
+        else:
+            await ep.send(msg)
+
+    listener = ucp.create_listener(
+        server_node,
+    )
+    await client_node(listener.port)
+
+    # del listener
+
+    # import gc
+    # gc.collect()
---
+++ python/tests_async/test_send_recv.py
@@ -0,0 +1,160 @@
+import functools
+
+import pytest
+
+import ucxx as ucp
+
+np = pytest.importorskip("numpy")
+
+msg_sizes = [2**i for i in range(0, 25, 4)]
+dtypes = ["|u1", "<i8", "f8"]
+
+
+def make_echo_server(create_empty_data):
+    """
+    Returns an echo server that calls the function `create_empty_data(nbytes)`
+    to create the data container.`
+    """
+
+    async def echo_server(ep):
+        """
+        Basic echo server for sized messages.
+        We expect the other endpoint to follow the pattern::
+        # size of the real message (in bytes)
+        >>> await ep.send(msg_size)
+        >>> await ep.send(msg)       # send the real message
+        >>> await ep.recv(responds)  # receive the echo
+        """
+        msg_size = np.empty(1, dtype=np.uint64)
+        await ep.recv(msg_size)
+        msg = create_empty_data(msg_size[0])
+        await ep.recv(msg)
+        await ep.send(msg)
+        await ep.close()
+
+    return echo_server
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("size", msg_sizes)
+async def test_send_recv_bytes(size):
+    msg = bytearray(b"m" * size)
+    msg_size = np.array([len(msg)], dtype=np.uint64)
+
+    listener = ucp.create_listener(make_echo_server(lambda n: bytearray(n)))
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+    await client.send(msg_size)
+    await client.send(msg)
+    resp = bytearray(size)
+    await client.recv(resp)
+    assert resp == msg
+    await client.close()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("size", msg_sizes)
+@pytest.mark.parametrize("dtype", dtypes)
+async def test_send_recv_numpy(size, dtype):
+    msg = np.arange(size, dtype=dtype)
+    msg_size = np.array([msg.nbytes], dtype=np.uint64)
+
+    listener = ucp.create_listener(
+        make_echo_server(lambda n: np.empty(n, dtype=np.uint8))
+    )
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+    await client.send(msg_size)
+    await client.send(msg)
+    resp = np.empty_like(msg)
+    await client.recv(resp)
+    np.testing.assert_array_equal(resp, msg)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("size", msg_sizes)
+@pytest.mark.parametrize("dtype", dtypes)
+async def test_send_recv_cupy(size, dtype):
+    cupy = pytest.importorskip("cupy")
+
+    msg = cupy.arange(size, dtype=dtype)
+    msg_size = np.array([msg.nbytes], dtype=np.uint64)
+
+    listener = ucp.create_listener(
+        make_echo_server(lambda n: cupy.empty((n,), dtype=np.uint8))
+    )
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+    await client.send(msg_size)
+    await client.send(msg)
+    resp = cupy.empty_like(msg)
+    await client.recv(resp)
+    np.testing.assert_array_equal(cupy.asnumpy(resp), cupy.asnumpy(msg))
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("size", msg_sizes)
+@pytest.mark.parametrize("dtype", dtypes)
+async def test_send_recv_numba(size, dtype):
+    cuda = pytest.importorskip("numba.cuda")
+
+    ary = np.arange(size, dtype=dtype)
+    msg = cuda.to_device(ary)
+    msg_size = np.array([msg.nbytes], dtype=np.uint64)
+    listener = ucp.create_listener(
+        make_echo_server(lambda n: cuda.device_array((n,), dtype=np.uint8))
+    )
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+    await client.send(msg_size)
+    await client.send(msg)
+    resp = cuda.device_array_like(msg)
+    await client.recv(resp)
+    np.testing.assert_array_equal(np.array(resp), np.array(msg))
+
+
+@pytest.mark.asyncio
+async def test_send_recv_error():
+    async def say_hey_server(ep):
+        await ep.send(bytearray(b"Hey"))
+        await ep.close()
+
+    listener = ucp.create_listener(say_hey_server)
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+
+    msg = bytearray(100)
+    with pytest.raises(
+        ucp.exceptions.UCXMsgTruncated,
+        match=r"length mismatch: 3 \(got\) != 100 \(expected\)",
+    ):
+        await client.recv(msg)
+
+
+@pytest.mark.asyncio
+async def test_send_recv_obj():
+    async def echo_obj_server(ep):
+        obj = await ep.recv_obj()
+        await ep.send_obj(obj)
+        await ep.close()
+
+    listener = ucp.create_listener(echo_obj_server)
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+
+    msg = bytearray(b"hello")
+    await client.send_obj(msg)
+    got = await client.recv_obj()
+    assert msg == got
+
+
+@pytest.mark.asyncio
+async def test_send_recv_obj_numpy():
+    allocator = functools.partial(np.empty, dtype=np.uint8)
+
+    async def echo_obj_server(ep):
+        obj = await ep.recv_obj(allocator=allocator)
+        await ep.send_obj(obj)
+        await ep.close()
+
+    listener = ucp.create_listener(echo_obj_server)
+    client = await ucp.create_endpoint(ucp.get_address(), listener.port)
+
+    msg = bytearray(b"hello")
+    await client.send_obj(msg)
+    got = await client.recv_obj(allocator=allocator)
+    assert msg == got
