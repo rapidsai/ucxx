12153dbac239a1b2d1252180efb8991411460bea
Peter Andreas Entschev <peter@entschev.com>
2022-11-18 14:22:06 -0800

    Split `tests/utils.h` in header and implementation

--- cpp/tests/utils.h
+++ cpp/tests/utils.h
@@ -7,6 +7,10 @@
 
 #include <functional>
 
+#include <cuda_runtime_api.h>
+
+#include <ucxx/api.h>
+
 enum class ProgressMode {
   Polling,
   Blocking,
@@ -15,45 +19,15 @@ enum class ProgressMode {
   ThreadBlocking,
 };
 
-void createCudaContextCallback(void* callbackArg)
-{
-  // Force CUDA context creation
-  cudaFree(0);
-}
+void createCudaContextCallback(void* callbackArg);
 
 void waitRequests(std::shared_ptr<ucxx::Worker> worker,
                   std::vector<std::shared_ptr<ucxx::Request>>& requests,
-                  std::function<void()>& progressWorker)
-{
-  for (auto& r : requests) {
-    do {
-      if (progressWorker) progressWorker();
-    } while (!r->isCompleted());
-    r->checkError();
-  }
-}
+                  std::function<void()>& progressWorker);
 
 void waitRequestsTagMulti(std::shared_ptr<ucxx::Worker> worker,
                           std::vector<std::shared_ptr<ucxx::RequestTagMulti>>& requests,
-                          std::function<void()>& progressWorker)
-{
-  for (auto& r : requests) {
-    do {
-      if (progressWorker) progressWorker();
-    } while (!r->isCompleted());
-    r->checkError();
-  }
-}
+                          std::function<void()>& progressWorker);
 
 std::function<void()> getProgressFunction(std::shared_ptr<ucxx::Worker> worker,
-                                          ProgressMode progressMode)
-{
-  if (progressMode == ProgressMode::Polling)
-    return std::bind(std::mem_fn(&ucxx::Worker::progress), worker);
-  else if (progressMode == ProgressMode::Blocking)
-    return std::bind(std::mem_fn(&ucxx::Worker::progressWorkerEvent), worker);
-  else if (progressMode == ProgressMode::Wait)
-    return std::bind(std::mem_fn(&ucxx::Worker::waitProgress), worker);
-  else
-    return std::function<void()>();
-}
+                                          ProgressMode progressMode);
