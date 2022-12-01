8ac2bce06a5cba70f043c90f5342712f802d4421
Peter Andreas Entschev <peter@entschev.com>
2022-12-01 13:45:54 -0800

    Add `ucxx::Listener` docs

--- cpp/include/ucxx/listener.h
+++ cpp/include/ucxx/listener.h
@@ -19,10 +19,25 @@ void ucpListenerDestructor(ucp_listener_h ptr);
 
 class Listener : public Component {
  private:
-  std::unique_ptr<ucp_listener, void (*)(ucp_listener_h)> _handle{nullptr, ucpListenerDestructor};
-  std::string _ip{};
-  uint16_t _port{0};
+  std::unique_ptr<ucp_listener, void (*)(ucp_listener_h)> _handle{
+    nullptr, ucpListenerDestructor};  ///< The UCP listener handle
+  std::string _ip{};                  ///< The IP address to which the listener is bound to
+  uint16_t _port{0};                  ///< The port to which the listener is bound to
 
+  /**
+   * @brief Private constructor of `ucxx::Listener`.
+   *
+   * This is the internal implementation of `ucxx::Listener` constructor, made private not
+   * to be called directly. Instead the user should call `worker::createListener()` or
+   * `ucxx::createListener()`.
+   *
+   *
+   * @param[in] worker        the worker from which to create the listener.
+   * @param[in] port          the port which the listener should be bound to.
+   * @param[in] callback      user-defined callback to be executed on incoming client
+   *                          connections.
+   * @param[in] callbackArgs  argument to be passed to the callback.
+   */
   Listener(std::shared_ptr<Worker> worker,
            uint16_t port,
            ucp_listener_conn_callback_t callback,
@@ -37,18 +52,106 @@ class Listener : public Component {
 
   ~Listener();
 
+  /**
+   * @brief Constructor of `shared_ptr<ucxx::Listener>`.
+   *
+   * The constructor for a `shared_ptr<ucxx::Listener>` object. The default constructor is
+   * made private to ensure all UCXX objects are shared pointers and correct lifetime
+   * management.
+   *
+   * @code{.cpp}
+   *
+   * typedef struct ClientContext {
+   *   std::shared_ptr<ucxx::Endpoint> endpoint{nullptr};
+   *   std::shared_ptr<ucxx::Listener> listener{nullptr};
+   * } ClientContextType;
+   *
+   * void myCallback(ucp_conn_request_h connRequest, void* arg) {
+   *   ClientContextType clientContext = (ClientContextType*);
+   *   clientContext->endpoint =
+   *     clientContext->listener->createEndpointFromConnRequest(connRequest);
+   * }
+   *
+   * ClientContext clientContext;
+   *
+   * // worker is `std::shared_ptr<ucxx::Worker>`
+   * auto listener = context->createListener(12345, myCallback, clientContext);
+   * clientContext->listener = listener;
+   *
+   * // Equivalent to line above
+   * // auto worker = ucxx::createWorker(context, false, false);
+   * @endcode
+   *
+   * @param[in] worker        the worker from which to create the listener.
+   * @param[in] port          the port which the listener should be bound to.
+   * @param[in] callback      user-defined callback to be executed on incoming client
+   *                          connections.
+   * @param[in] callbackArgs  argument to be passed to the callback.
+   *
+   * @returns The `shared_ptr<ucxx::Listener>` object.
+   */
   friend std::shared_ptr<Listener> createListener(std::shared_ptr<Worker> worker,
                                                   uint16_t port,
                                                   ucp_listener_conn_callback_t callback,
                                                   void* callbackArgs);
 
+  /**
+   * @brief Constructor for `shared_ptr<ucxx::Endpoint>`.
+   *
+   * The constructor for a `shared_ptr<ucxx::Endpoint>` object from a `ucp_conn_request_h`,
+   * as delivered by a `ucxx::Listener` connection callback.
+   *
+   * @code{.cpp}
+   * // worker is `std::shared_ptr<ucxx::Worker>`, with a `ucp_conn_request_h` delivered
+   * // by a `ucxx::Listener` connection callback.
+   * auto endpoint = listener->createEndpointFromConnRequest(connRequest, true);
+   *
+   * // Equivalent to line above
+   * // auto endpoint = ucxx::createEndpointFromConnRequest(listener, connRequest, true);
+   * @endcode
+   *
+   * @param[in] connRequest           handle to connection request delivered by a
+   *                                  listener callback.
+   * @param[in] endpointErrorHandling whether to enable endpoint error handling.
+   *
+   * @returns The `shared_ptr<ucxx::Endpoint>` object.
+   */
   std::shared_ptr<Endpoint> createEndpointFromConnRequest(ucp_conn_request_h connRequest,
                                                           bool endpointErrorHandling = true);
 
+  /**
+   * @brief Get the underlying `ucp_listener_h` handle.
+   *
+   * Lifetime of the `ucp_listener_h` handle is managed by the `ucxx::Listener` object and
+   * its ownership is non-transferrable. Once the `ucxx::Listener` is destroyed the handle
+   * is not valid anymore, it is the user's responsibility to ensure the owner's lifetime
+   * while using the handle.
+   *
+   * @code{.cpp}
+   * // listener is `std::shared_ptr<ucxx::Listener>`
+   * ucp_listener_h listenerHandle = listener->getHandle();
+   * @endcode
+   *
+   * @returns The underlying `ucp_listener_h` handle.
+   */
   ucp_listener_h getHandle();
 
+  /**
+   * @brief Get the port to which the listener is bound to.
+   *
+   * Get the port to which the listener is bound to.
+   *
+   * @returns the port to which the listener is bound to.
+   */
   uint16_t getPort();
 
+  /**
+   * @brief Get the IP address to which the listener is bound to.
+   *
+   * Get the IP address to which the listener is bound to.
+   *
+   * @returns the IP address to which the listener is bound to.
+   */
   std::string getIp();
 };
 
