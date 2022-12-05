c1541246c808dad017530373d697af0c1bfd089f
Peter Andreas Entschev <peter@entschev.com>
2022-12-05 04:37:38 -0800

    Add docs for C++ functions of Python exceptions

--- cpp/include/ucxx/python/exception.h
+++ cpp/include/ucxx/python/exception.h
@@ -22,10 +22,33 @@ extern PyObject* UCXXConfigError;
 extern PyObject* UCXXConnectionResetError;
 extern PyObject* UCXXMessageTruncatedError;
 
+/**
+ * @brief Create Python exceptions.
+ *
+ * Create UCXX-specific Python exceptions such that they are visible both from C/C++ and
+ * Python.
+ */
 void create_exceptions();
 
+/**
+ * @brief Raise a C++ exception in Python.
+ *
+ * Raise a C++ exception in Python. When a C++ exception occurs, Python needs to be able
+ * to be informed of such event and be able to raise a Python exception from it. This
+ * function raises both general C++ exceptions, as well as UCXX-specific exceptions.
+ *
+ * To use this, C++ methods and functions that are exposed to Python via Cython must have
+ * a `except +raise_py_error` as a declaration suffix.
+ */
 void raise_py_error();
 
+/**
+ * @brief Get a Python exception from UCS status.
+ *
+ * Given a UCS status, get a matching Python exception object.
+ *
+ * @param[in] status UCS status from which to get exception.
+ */
 PyObject* get_python_exception_from_ucs_status(ucs_status_t status);
 
 }  // namespace python
