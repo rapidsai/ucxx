0720ba0da6a79a83744e8b11050f8c02e73282d1
Peter Andreas Entschev <peter@entschev.com>
2023-03-10 12:54:33 -0800

    Remove Python 3.7

--- python/setup.py
+++ python/setup.py
@@ -115,10 +115,9 @@ setup(
         "Topic :: System :: Hardware",
         "Topic :: System :: Systems Administration",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     url="https://github.com/rapidsai/ucx-py",
 )
