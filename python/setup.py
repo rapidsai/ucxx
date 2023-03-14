90cda8a309791ee2fb72f4e24a8ec5b84f3dca92
Peter Andreas Entschev <peter@entschev.com>
2023-03-14 11:00:07 -0700

    Update to Python package build to scikit-build

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,7 @@
-# Copyright (c) 2018-2021, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
 # See file LICENSE for terms.
+
+from __future__ import absolute_import, print_function
 
 import os
 import re
@@ -7,16 +9,10 @@ from distutils.sysconfig import get_config_var, get_python_inc
 
 import numpy as np
 from Cython.Build import cythonize
-from Cython.Distutils.build_ext import new_build_ext as build_ext
-from setuptools import find_packages, setup
+from Cython.Distutils.build_ext import new_build_ext
+from setuptools import setup
 from setuptools.extension import Extension
 
-import versioneer
-
-with open("../README.md", "r") as fh:
-    readme = fh.read()
-
-setup_dir = os.path.dirname(os.path.realpath(__file__))
 include_dirs = [
     os.path.dirname(get_python_inc()),
     np.get_include(),
@@ -27,11 +23,10 @@ libraries = ["ucxx", "ucxx_python", "fmt"]
 cpp_extra_compile_args = [
     "-std=c++17",
     "-Werror",
-    "-g",
     "-DUCXX_ENABLE_PYTHON=1",
     "-DUCXX_ENABLE_RMM=1",
 ]
-c_extra_compile_args = ["-Werror", "-g"]
+c_extra_compile_args = ["-Werror"]
 depends = []
 
 
@@ -75,49 +70,7 @@ ext_modules = cythonize(
     compile_time_env={"CY_UCP_AM_SUPPORTED": _am_supported},
 )
 
-cmdclass = dict()
-cmdclass.update(versioneer.get_cmdclass())
-cmdclass["build_ext"] = build_ext
-
-install_requires = [
-    "numpy",
-    "pynvml",
-]
-
-tests_require = [
-    "pytest",
-    "pytest-asyncio",
-]
-
 setup(
-    name="ucxx",
-    packages=find_packages(exclude=["tests*"]),
-    package_data={"": ["*.pyi"]},
     ext_modules=ext_modules,
-    cmdclass=cmdclass,
-    version=versioneer.get_version(),
-    python_requires=">=3.6",
-    install_requires=install_requires,
-    tests_require=tests_require,
-    description="Python Bindings for the Unified Communication X library (UCX)",
-    long_description=readme,
-    author="NVIDIA Corporation",
-    license="BSD-3-Clause",
-    license_files=["LICENSE"],
-    zip_safe=False,
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Hardware",
-        "Topic :: System :: Systems Administration",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    url="https://github.com/rapidsai/ucx-py",
+    cmdclass={"build_ext": new_build_ext},
 )
