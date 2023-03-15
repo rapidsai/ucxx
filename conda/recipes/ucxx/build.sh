845064912ed27784c52e4ab574d1a9d25a20fe91
Peter Andreas Entschev <peter@entschev.com>
2023-03-15 12:47:46 -0700

    Remove duplicate install

--- conda/recipes/ucxx/build.sh
+++ conda/recipes/ucxx/build.sh
@@ -3,5 +3,4 @@
 # This assumes the script is executed from the root of the repo directory
 cd python/
 PARALLEL_LEVEL=${PARALLEL_LEVEL:=$(nproc)}
-PARALLEL_LEVEL=${PARALLEL_LEVEL} python setup.py build_ext --inplace -j${PARALLEL_LEVEL}
 $PYTHON -m pip install -vv --no-deps --ignore-installed . --global-option="build_ext" --global-option="-j${PARALLEL_LEVEL}"
