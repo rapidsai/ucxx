db8da809fe230d0fb51c6ac81e78beaf27ab9ff4
Peter Andreas Entschev <peter@entschev.com>
2023-03-23 08:51:39 -0700

    Use `build.sh` in Python conda recipe

--- conda/recipes/ucxx/build.sh
+++ conda/recipes/ucxx/build.sh
@@ -1,6 +1,4 @@
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# Copyright (c) 2023, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
-cd python/
-PARALLEL_LEVEL=${PARALLEL_LEVEL:=$(nproc)}
-$PYTHON -m pip install -vv --no-deps --ignore-installed . --global-option="build_ext" --global-option="-j${PARALLEL_LEVEL}"
+./build.sh ucxx
