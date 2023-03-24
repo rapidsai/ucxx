d4e6dc97e8fb3fe9d12651f112c2cc9a80552c5a
Peter Andreas Entschev <peter@entschev.com>
2023-03-24 13:05:08 -0700

    Fix building libucxx-examples conda package

--- conda/recipes/libucxx/build.sh
+++ conda/recipes/libucxx/build.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
 export ucxx_ROOT="$(realpath ./cpp/build)"
-./build.sh -n -v libucxx libucxx_python benchmarks tests --build_metrics --incl_cache_stats --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+./build.sh -n -v libucxx libucxx_python benchmarks examples tests --build_metrics --incl_cache_stats --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
