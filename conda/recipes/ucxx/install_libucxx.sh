1fde27c7e4c624b86518c0e3b839a34c4258c362
jakirkham <jakirkham@gmail.com>
2023-04-06 11:27:02 -0700

    Remove RAPIDS headers from `libucxx`
    This is a workaround to remove RAPIDS headers from `libucxx`. Ideally
    they shouldn't be installed in the first place. However for now just
    remove them to avoid issues.

--- conda/recipes/ucxx/install_libucxx.sh
+++ conda/recipes/ucxx/install_libucxx.sh
@@ -5,3 +5,8 @@
 
 cmake --install cpp/build
 cmake --install cpp/build --component benchmarks
+
+# For some reason RAPIDS headers are getting installed causing clobbering, which shouldn't happen.
+# To workaround this issue for now, just remove all the RAPIDS headers that were installed to avoid clobbering.
+# xref: https://github.com/rapidsai/ucxx/issues/20
+rm -rf "${PREFIX}/include/rapids"
