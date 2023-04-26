465ddef4c124410e0492a0d2518faa4a67183b20
Peter Andreas Entschev <peter@entschev.com>
2023-04-26 11:40:15 -0700

    Update CI release script

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ########################
-# ucxx Version Updater #
+# UCXX Version Updater #
 ########################
 
 ## Usage
@@ -24,8 +24,13 @@ NEXT_MINOR=$(echo $NEXT_FULL_TAG | awk '{split($0, a, "."); print a[2]}')
 NEXT_SHORT_TAG=${NEXT_MAJOR}.${NEXT_MINOR}
 
 # Get RAPIDS version associated w/ ucx-py version
+CURRENT_RAPIDS_VERSION="$(curl -sL https://version.gpuci.io/ucx-py/${CURRENT_SHORT_TAG})"
 NEXT_RAPIDS_VERSION="$(curl -sL https://version.gpuci.io/ucx-py/${NEXT_SHORT_TAG})"
 
+# Need to distutils-normalize the versions for some use cases
+CURRENT_SHORT_TAG_PEP440=$(python -c "from setuptools.extern import packaging; print(packaging.version.Version('${CURRENT_RAPIDS_VERSION}'))")
+NEXT_SHORT_TAG_PEP440=$(python -c "from setuptools.extern import packaging; print(packaging.version.Version('${NEXT_RAPIDS_VERSION}'))")
+echo "Current RAPIDS is ${CURRENT_SHORT_TAG_PEP440}, next is ${NEXT_SHORT_TAG_PEP440}"
 
 echo "Preparing release $CURRENT_TAG => $NEXT_FULL_TAG"
 
@@ -34,9 +39,16 @@ function sed_runner() {
     sed -i.bak ''"$1"'' $2 && rm -f ${2}.bak
 }
 
-sed_runner "s/cudf=.*/cudf=${NEXT_RAPIDS_VERSION}/g" dependencies.yaml
-sed_runner "s/rmm=.*/rmm=${NEXT_RAPIDS_VERSION}/g" dependencies.yaml
-sed_runner "s/rmm =.*/rmm =${NEXT_RAPIDS_VERSION}/g" conda/recipes/ucxx/conda_build_config.yaml
+# bump cudf, dask-cuda and rmm
+sed_runner "s/rmm =${CURRENT_RAPIDS_VERSION}/rmm =${NEXT_RAPIDS_VERSION}/g" conda/recipes/ucxx/meta.yaml
+for FILE in conda/environments/*.yaml dependencies.yaml; do
+  sed_runner "s/cudf==${CURRENT_SHORT_TAG_PEP440}/cudf==${NEXT_SHORT_TAG_PEP440}/g" ${FILE};
+  sed_runner "s/dask-cuda==${CURRENT_SHORT_TAG_PEP440}/dask-cuda==${NEXT_SHORT_TAG_PEP440}/g" ${FILE};
+  sed_runner "s/rmm==${CURRENT_SHORT_TAG_PEP440}/rmm==${NEXT_SHORT_TAG_PEP440}/g" ${FILE};
+done
+
+# rapids-cmake version
+sed_runner 's/'"branch-.*\/RAPIDS.cmake"'/'"branch-${NEXT_RAPIDS_VERSION}\/RAPIDS.cmake"'/g' fetch_rapids.cmake
 
 for FILE in .github/workflows/*.yaml; do
   sed_runner "/shared-action-workflows/ s/@.*/@branch-${NEXT_RAPIDS_VERSION}/g" "${FILE}"
