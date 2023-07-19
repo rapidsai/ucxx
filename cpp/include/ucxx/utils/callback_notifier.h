fa1b5717dba192342f196681828a45d9f4c1adea
Peter Andreas Entschev <peter@entschev.com>
2023-07-19 02:40:56 -0700

    Update `CallbackNotifier` documentation

--- cpp/include/ucxx/utils/callback_notifier.h
+++ cpp/include/ucxx/utils/callback_notifier.h
@@ -21,9 +21,13 @@ class CallbackNotifier {
 
  public:
   /**
-   * @brief Construct a thread-safe notification object with given initial value
+   * @brief Construct a thread-safe notification object with given initial value.
    *
-   * @param[in] init   The initial flag value
+   * Construct a thread-safe notification object with a given initial value which may be
+   * later set via `store()` in one thread and block another thread running `wait()` while
+   * the new value is not set.
+   *
+   * @param[in] init  The initial flag value
    */
   explicit CallbackNotifier(Flag flag) : _flag{flag} {}
 
@@ -35,9 +39,11 @@ class CallbackNotifier {
   CallbackNotifier& operator=(CallbackNotifier&& o) = delete;
 
   /**
-   * @brief Store a new flag value and notify a waiting thread
+   * @brief Store a new flag value and notify a waiting thread.
    *
-   * @param[in] flag   New flag value
+   * Store a new flag value and notify another thread blocked by a call to `wait()`.
+   *
+   * @param[in] flag  The new flag value.
    */
   void store(Flag flag)
   {
@@ -49,12 +55,15 @@ class CallbackNotifier {
   }
 
   /**
-   * @brief Wait while predicate is not true for the flag value to change
+   * @brief Wait while predicate is not true for the flag value to change.
    *
-   * @param[in] p   Function of type T -> bool called with the flag
-   *                value. This function loops until the predicate is
-   *                satisfied. See also std::condition_variable::wait
-   * @param[out]    The new flag value
+   * Wait while predicate is not true which should be satisfied by a change in the flag's
+   * value by a `store()` call on a different thread.
+   *
+   * @param[in] compare Function of type `T -> bool` called with the flag value. This
+   *                    function loops until the predicate is satisfied. See also
+   *                    `std::condition_variable::wait`.
+   * @param[out]        The new flag value.
    */
   template <typename Compare>
   Flag wait(Compare compare)
