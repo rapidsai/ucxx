e4b928d8dc5203e13b1ab56fd5584a708b5ea387
Peter Andreas Entschev <peter@entschev.com>
2023-11-13 02:51:11 -0800

    Move `PythonFutureTask` implementation details
    Moving implementation details of `PythonFutureTask` into a new internal
    class has the benefit of allowing default move constructor and
    assignment operator to exist.

--- cpp/python/include/ucxx/python/python_future_task.h
+++ cpp/python/include/ucxx/python/python_future_task.h
@@ -24,9 +24,11 @@ namespace ucxx {
 
 namespace python {
 
+namespace detail {
+
 template <typename ReturnType, typename... TaskArgs>
-class PythonFutureTask : public std::enable_shared_from_this<PythonFutureTask<ReturnType>> {
- private:
+struct PythonFutureTask {
+ public:
   std::packaged_task<ReturnType(TaskArgs...)> _task{};  ///< The user-defined C++ task to run
   std::function<PyObject*(ReturnType)>
     _pythonConvert{};                 ///< Function to convert the C++ result into Python value
@@ -34,6 +36,7 @@ class PythonFutureTask : public std::enable_shared_from_this<PythonFutureTask<Re
   PyObject* _handle{};                ///< The handle to the Python future
   std::future<ReturnType> _future{};  ///< The C++ future containing the task result
 
+ private:
   /**
    * @brief Set the result value of the Python future.
    *
@@ -111,49 +114,32 @@ class PythonFutureTask : public std::enable_shared_from_this<PythonFutureTask<Re
   }
 
  public:
-  /**
-   * @brief Construct a Python future backed by C++ `std::packaged_task`.
-   *
-   * Construct a future object that receives a user-defined C++ `std::packaged_task` which
-   * runs asynchronously using an internal `std::async` that ultimately notifies a Python
-   * future that can be awaited in Python code.
-   *
-   * Note that this call will take the Python GIL and requires that the current thread have
-   * an asynchronous event loop set.
-   *
-   * @param[in] task the user-defined C++ task.
-   * @param[in] pythonConvert C-Python function to convert a C object into a `PyObject*`
-   *                          representing the result of the task.
-   * @param[in] asyncioEventLoop pointer to a valid Python object containing the event loop
-   *                             that the application is using, to which the Python future
-   *                             will belong to.
-   * @param[in] launchPolicy launch policy for the async C++ task.
-   */
   explicit PythonFutureTask(std::packaged_task<ReturnType(TaskArgs...)> task,
                             std::function<PyObject*(ReturnType)> pythonConvert,
                             PyObject* asyncioEventLoop,
                             std::launch launchPolicy = std::launch::async)
     : _task{std::move(task)},
-      _pythonConvert(pythonConvert),
+      _pythonConvert(std::move(pythonConvert)),
       _asyncioEventLoop(asyncioEventLoop),
-      _handle{ucxx::python::create_python_future_with_event_loop(asyncioEventLoop)}
+      _handle{ucxx::python::create_python_future_with_event_loop(asyncioEventLoop)},
+      _future{std::async(launchPolicy, [this]() {
+        std::future<ReturnType> result = this->_task.get_future();
+        this->_task();
+        try {
+          const ReturnType r = result.get();
+          this->setResult(r);
+          return r;
+        } catch (std::exception& e) {
+          this->setException(e);
+        }
+      })}
   {
-    _future = std::async(launchPolicy, [this]() {
-      std::future<ReturnType> result = this->_task.get_future();
-      this->_task();
-      try {
-        const ReturnType r = result.get();
-        this->setResult(r);
-        return r;
-      } catch (std::exception& e) {
-        this->setException(e);
-      }
-    });
   }
+
   PythonFutureTask(const PythonFutureTask&)            = delete;
   PythonFutureTask& operator=(PythonFutureTask const&) = delete;
-  PythonFutureTask(PythonFutureTask&& o)               = delete;
-  PythonFutureTask& operator=(PythonFutureTask&& o)    = delete;
+  PythonFutureTask(PythonFutureTask&& o)               = default;
+  PythonFutureTask& operator=(PythonFutureTask&& o)    = default;
 
   /**
    * @brief Python future destructor.
@@ -169,10 +155,11 @@ class PythonFutureTask : public std::enable_shared_from_this<PythonFutureTask<Re
    *
    * @returns The underlying C++ future.
    */
-  std::future<ReturnType>& getFuture() { 
+  std::future<ReturnType>& getFuture()
+  {
     if (_handle == nullptr) throw std::runtime_error("Invalid object or already released");
-    
-    return _future; 
+
+    return _future;
   }
 
   /**
@@ -213,6 +200,92 @@ class PythonFutureTask : public std::enable_shared_from_this<PythonFutureTask<Re
 
     return std::exchange(_handle, nullptr);
   }
+};
+
+}  // namespace detail
+
+template <typename ReturnType, typename... TaskArgs>
+class PythonFutureTask : public std::enable_shared_from_this<PythonFutureTask<ReturnType>> {
+ private:
+  std::unique_ptr<detail::PythonFutureTask<ReturnType, TaskArgs...>> _detail{
+    nullptr};  ///< internal C++ task/future manager
+
+ public:
+  /**
+   * @brief Construct a Python future backed by C++ `std::packaged_task`.
+   *
+   * Construct a future object that receives a user-defined C++ `std::packaged_task` which
+   * runs asynchronously using an internal `std::async` that ultimately notifies a Python
+   * future that can be awaited in Python code.
+   *
+   * Note that this call will take the Python GIL and requires that the current thread have
+   * an asynchronous event loop set.
+   *
+   * @param[in] task the user-defined C++ task.
+   * @param[in] pythonConvert C-Python function to convert a C object into a `PyObject*`
+   *                          representing the result of the task.
+   * @param[in] asyncioEventLoop pointer to a valid Python object containing the event loop
+   *                             that the application is using, to which the Python future
+   *                             will belong to.
+   * @param[in] launchPolicy launch policy for the async C++ task.
+   */
+  explicit PythonFutureTask(std::packaged_task<ReturnType(TaskArgs...)> task,
+                            std::function<PyObject*(ReturnType)> pythonConvert,
+                            PyObject* asyncioEventLoop,
+                            std::launch launchPolicy = std::launch::async)
+    : _detail(std::make_unique<detail::PythonFutureTask<ReturnType, TaskArgs...>>(
+        std::move(task), std::move(pythonConvert), asyncioEventLoop, launchPolicy))
+  {
+  }
+  PythonFutureTask(const PythonFutureTask&)            = delete;
+  PythonFutureTask& operator=(PythonFutureTask const&) = delete;
+  PythonFutureTask(PythonFutureTask&& o)               = default;
+  PythonFutureTask& operator=(PythonFutureTask&& o)    = default;
+
+  /**
+   * @brief Python future destructor.
+   *
+   * Decrement the reference count of the underlying Python future.
+   */
+  ~PythonFutureTask() {}
+
+  /**
+   * @brief Get the C++ future.
+   *
+   * Get the underlying C++ future that can be awaited and have its result read.
+   *
+   * @returns The underlying C++ future.
+   */
+  std::future<ReturnType>& getFuture() { return _detail->getFuture(); }
+
+  /**
+   * @brief Get the underlying future `PyObject*` handle but does not release ownership.
+   *
+   * Get the underlying `PyObject*` handle without releasing ownership. This can be useful
+   * for example for logging, where we want to see the address of the pointer but do not
+   * want to transfer ownership.
+   *
+   * @warning The destructor will also destroy the Python future, a pointer taken via this
+   * method will cause the object to become invalid.
+   *
+   * @throws std::runtime_error if the object is invalid or has been already released.
+   *
+   * @returns The underlying `PyObject*` handle.
+   */
+  PyObject* getHandle() { return _detail->getHandle(); }
+
+  /**
+   * @brief Get the underlying future `PyObject*` handle and release ownership.
+   *
+   * Get the underlying `PyObject*` handle releasing ownership. This should be used when
+   * the future needs to be permanently transferred to Python code. After calling this
+   * method the object becomes invalid for any other uses.
+   *
+   * @throws std::runtime_error if the object is invalid or has been already released.
+   *
+   * @returns The underlying `PyObject*` handle.
+   */
+  PyObject* release() { return _detail->release(); }
 };
 
 }  // namespace python
