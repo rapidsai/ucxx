b9c5837d08eeb0e75f62c2564cf27e9540e30d80
Bradley Dice <bdice@bradleydice.com>
2023-11-29 16:17:49 -0800

    Use cudf artifacts.

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -91,11 +91,15 @@ CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
 LIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1374 cpp)
 RMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1374 python)
+LIBCUDF_CHANNEL=$(rapids-get-pr-conda-artifact cudf 14355 cpp)
+CUDF_CHANNEL=$(rapids-get-pr-conda-artifact cudf 14355 python)
 
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
   --channel "${LIBRMM_CHANNEL}" \
   --channel "${RMM_CHANNEL}" \
+  --channel "${LIBCUDF_CHANNEL}" \
+  --channel "${CUDF_CHANNEL}" \
   libucxx ucxx distributed-ucxx
 
 # TODO: Perhaps install from conda? We need distributed installed in developer
