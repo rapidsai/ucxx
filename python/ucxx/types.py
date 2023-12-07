f673cdc13453af5e3a90416720c5eeedecb684c6
Peter Andreas Entschev <peter@entschev.com>
2023-12-07 01:44:35 -0800

    Add missing `types.py`

---
+++ python/ucxx/types.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from ._lib.libucxx import UCXXTag, UCXXTagMask, UCXXTagMaskFull  # noqa
+
+Tag = UCXXTag
+TagMask = UCXXTagMask
+TagMaskFull = UCXXTagMaskFull
