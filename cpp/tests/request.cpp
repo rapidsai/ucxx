c644dc6ee77d4871c00e02e1bfe457ddee45d8d0
Peter Andreas Entschev <peter@entschev.com>
2023-12-11 02:52:32 -0800

    Test zero-sized messages

--- cpp/tests/request.cpp
+++ cpp/tests/request.cpp
@@ -194,15 +194,20 @@ TEST_P(RequestTest, ProgressStream)
   allocate();
 
   // Submit and wait for transfers to complete
-  std::vector<std::shared_ptr<ucxx::Request>> requests;
-  requests.push_back(_ep->streamSend(_sendPtr[0], _messageSize, 0));
-  requests.push_back(_ep->streamRecv(_recvPtr[0], _messageSize, 0));
-  waitRequests(_worker, requests, _progressWorker);
+  if (_messageSize == 0) {
+    EXPECT_THROW(_ep->streamSend(_sendPtr[0], _messageSize, 0), std::runtime_error);
+    EXPECT_THROW(_ep->streamRecv(_recvPtr[0], _messageSize, 0), std::runtime_error);
+  } else {
+    std::vector<std::shared_ptr<ucxx::Request>> requests;
+    requests.push_back(_ep->streamSend(_sendPtr[0], _messageSize, 0));
+    requests.push_back(_ep->streamRecv(_recvPtr[0], _messageSize, 0));
+    waitRequests(_worker, requests, _progressWorker);
 
-  copyResults();
+    copyResults();
 
-  // Assert data correctness
-  ASSERT_THAT(_recv[0], ContainerEq(_send[0]));
+    // Assert data correctness
+    ASSERT_THAT(_recv[0], ContainerEq(_send[0]));
+  }
 }
 
 TEST_P(RequestTest, ProgressTag)
@@ -311,7 +316,7 @@ INSTANTIATE_TEST_SUITE_P(ProgressModes,
                                         // ProgressMode::Wait,  // Hangs on Stream
                                         ProgressMode::ThreadPolling,
                                         ProgressMode::ThreadBlocking),
-                                 Values(1, 1024, 2048, 1048576)));
+                                 Values(0, 1, 1024, 2048, 1048576)));
 
 INSTANTIATE_TEST_SUITE_P(DelayedSubmission,
                          RequestTest,
@@ -319,7 +324,7 @@ INSTANTIATE_TEST_SUITE_P(DelayedSubmission,
                                  Values(false),
                                  Values(true),
                                  Values(ProgressMode::ThreadPolling, ProgressMode::ThreadBlocking),
-                                 Values(1, 1024, 2048, 1048576)));
+                                 Values(0, 1, 1024, 2048, 1048576)));
 
 #if UCXX_ENABLE_RMM
 INSTANTIATE_TEST_SUITE_P(RMMProgressModes,
@@ -332,7 +337,7 @@ INSTANTIATE_TEST_SUITE_P(RMMProgressModes,
                                         // ProgressMode::Wait,  // Hangs on Stream
                                         ProgressMode::ThreadPolling,
                                         ProgressMode::ThreadBlocking),
-                                 Values(1, 1024, 2048, 1048576)));
+                                 Values(0, 1, 1024, 2048, 1048576)));
 
 INSTANTIATE_TEST_SUITE_P(RMMDelayedSubmission,
                          RequestTest,
@@ -340,7 +345,7 @@ INSTANTIATE_TEST_SUITE_P(RMMDelayedSubmission,
                                  Values(false, true),
                                  Values(true),
                                  Values(ProgressMode::ThreadPolling, ProgressMode::ThreadBlocking),
-                                 Values(1, 1024, 2048, 1048576)));
+                                 Values(0, 1, 1024, 2048, 1048576)));
 #endif
 
 }  // namespace
--- python/ucxx/_lib/tests/test_server_client.py
+++ python/ucxx/_lib/tests/test_server_client.py
@@ -80,6 +80,13 @@ def _echo_server(get_queue, put_queue, transfer_api, msg_size, progress_mode):
 
     msg = Array(bytearray(msg_size))
 
+    if transfer_api == "stream" and msg_size == 0:
+        with pytest.raises(RuntimeError):
+            _recv(ep[0], transfer_api, msg)
+        with pytest.raises(RuntimeError):
+            _send(ep[0], transfer_api, msg)
+        return
+
     # We reuse the message buffer, so we must receive, wait, and then send
     # it back again.
     requests = [_recv(ep[0], transfer_api, msg)]
@@ -131,6 +138,14 @@ def _echo_client(transfer_api, msg_size, progress_mode, port):
 
     send_msg = bytes(os.urandom(msg_size))
     recv_msg = bytearray(msg_size)
+
+    if transfer_api == "stream" and msg_size == 0:
+        with pytest.raises(RuntimeError):
+            _send(ep, transfer_api, Array(send_msg))
+        with pytest.raises(RuntimeError):
+            _recv(ep, transfer_api, Array(recv_msg))
+        return
+
     requests = [
         _send(ep, transfer_api, Array(send_msg)),
         _recv(ep, transfer_api, Array(recv_msg)),
@@ -146,7 +161,7 @@ def _echo_client(transfer_api, msg_size, progress_mode, port):
 
 
 @pytest.mark.parametrize("transfer_api", ["am", "stream", "tag"])
-@pytest.mark.parametrize("msg_size", [10, 2**24])
+@pytest.mark.parametrize("msg_size", [0, 10, 2**24])
 @pytest.mark.parametrize("progress_mode", ["blocking", "thread"])
 def test_server_client(transfer_api, msg_size, progress_mode):
     put_queue, get_queue = mp.Queue(), mp.Queue()
