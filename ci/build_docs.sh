e6e7c2893f24bf0b1630deb173f36e4139da2948
Peter Andreas Entschev <peter@entschev.com>
2024-01-12 07:17:59 -0800

    Add doxygen pre-commit hooks, dependencies and CI build files

---
+++ ci/build_docs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright (c) 2023, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+rapids-logger "Create test conda environment"
+. /opt/conda/etc/profile.d/conda.sh
+
+ENV_YAML_DIR="$(mktemp -d)"
+
+rapids-dependency-file-generator \
+  --output conda \
+  --file_key docs \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee "${ENV_YAML_DIR}/env.yaml"
+
+rapids-mamba-retry env create --force -f "${ENV_YAML_DIR}/env.yaml" -n docs
+conda activate docs
+
+rapids-print-env
+
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
+
+rapids-mamba-retry install \
+  --channel "${CPP_CHANNEL}" \
+  --channel "${PYTHON_CHANNEL}" \
+  libcudf cudf dask-cudf
+
+export RAPIDS_DOCS_DIR="$(mktemp -d)"
+
+rapids-logger "Build CPP docs"
+pushd cpp/doxygen
+doxygen Doxyfile
+mkdir -p "${RAPIDS_DOCS_DIR}/libucxx/html"
+mv html/* "${RAPIDS_DOCS_DIR}/libucxx/html"
+popd
+
+rapids-upload-docs
