fc77f1f06af9c12d10a8722e5962069c2a257b57
Peter Andreas Entschev <peter@entschev.com>
2024-01-17 19:09:35 +0100

    Do not break line on single variable `cdef`s
    Co-authored-by: Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>

--- python/ucxx/_lib/tag.pyx
+++ python/ucxx/_lib/tag.pyx
@@ -6,16 +6,14 @@ from libc.stdint cimport uint64_t
 
 
 cdef class UCXXTag:
-    cdef:
-        readonly uint64_t value
+    cdef readonly uint64_t value
 
     def __cinit__(self, uint64_t tag) -> None:
         self.value = tag
 
 
 cdef class UCXXTagMask:
-    cdef:
-        readonly uint64_t value
+    cdef readonly uint64_t value
 
     def __cinit__(self, uint64_t tag_mask) -> None:
         self.value = tag_mask
