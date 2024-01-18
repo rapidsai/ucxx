17adea4ecaaf580bce1f4ac1a699b4b035aaf666
Peter Andreas Entschev <peter@entschev.com>
2024-01-18 14:12:44 -0800

    Package name fixes

--- ci/build_wheel_ucxx.sh
+++ ci/build_wheel_ucxx.sh
@@ -9,4 +9,4 @@ export RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION}
 
 ./ci/build_wheel.sh ucxx ${package_dir}
 
-RAPIDS_PY_WHEEL_NAME="ucxx_${AUDITWHEEL_POLICY}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 ${package_dir}/final_dist
+RAPIDS_PY_WHEEL_NAME="ucxx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 ${package_dir}/final_dist
--- ci/test_wheel_distributed_ucxx.sh
+++ ci/test_wheel_distributed_ucxx.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-export PROJECT_NAME="distributed-ucxx"
+export PROJECT_NAME="distributed_ucxx"
 
 source "$(dirname "$0")/test_utils.sh"
 source "$(dirname "$0")/test_common.sh"
