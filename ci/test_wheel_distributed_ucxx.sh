199cb2511100bc5ada0e3c55bb329e877bfbf52a
Bradley Dice <bdice@bradleydice.com>
2024-01-19 12:44:33 -0600

    Install ucxx from this build when testing distributed-ucxx.

--- ci/test_wheel_distributed_ucxx.sh
+++ ci/test_wheel_distributed_ucxx.sh
@@ -12,10 +12,9 @@ mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="${PROJECT_NAME}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# On arm also need to install CMake because treelite needs to be compiled (no wheels available for arm).
-if [[ "$(arch)" == "aarch64" ]]; then
-    python -m pip install cmake
-fi
+# Install previously built ucxx wheel
+RAPIDS_PY_WHEEL_NAME="ucxx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-ucxx-dep
+python -m pip install --no-deps ./local-ucxx-dep/ucxx*.whl
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/${PROJECT_NAME}*.whl)[test]
