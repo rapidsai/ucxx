d4d93cdbc8d8a41bbe6fffca6c2e5281bb1e2094
James Lamb <jlamb@nvidia.com>
2024-01-23 09:31:04 -0600

    fix pyproject.toml parsing

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -53,9 +53,10 @@ DEPENDENCIES=(
   rmm
 )
 for DEP in "${DEPENDENCIES[@]}"; do
-  for FILE in dependencies.yaml conda/environments/*.yaml python/pyproject.toml; do
+  for FILE in dependencies.yaml conda/environments/*.yaml; do
     sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*/g" "${FILE}"
   done;
+  sed_runner "/\"${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*\"/==${NEXT_SHORT_TAG_PEP440}\.*\"/g" python/pyproject.toml;
 done
 
 # rapids-cmake version
