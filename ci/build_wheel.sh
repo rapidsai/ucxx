4fa91fb5d201958d3f63a84debec5a6aa3a5e36b
Peter Andreas Entschev <peter@entschev.com>
2024-01-31 15:49:21 +0100

    Make wheel build script more consistent
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -36,9 +36,7 @@ if [[ ${package_name} == "distributed-ucxx" ]]; then
     sed -r -i "s/rapids-dask-dependency==(.*)\"/rapids-dask-dependency==\1${alpha_spec}\"/g" ${pyproject_file}
     sed -r -i "s/\"ucxx(.*)\"/\"ucxx${PACKAGE_CUDA_SUFFIX}\1${alpha_spec}\"/g" ${pyproject_file}
 
-    cd "${package_dir}"
-
-    python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+    python -m pip wheel "${package_dir}/" -w "${package_dir}/dist" -vvv --no-deps --disable-pip-version-check
 elif [[ ${package_name} == "ucxx" ]]; then
     # Add -cuXX to package name
     sed -r -i "s/rapids-dask-dependency==(.*)\"/rapids-dask-dependency==\1${alpha_spec}\"/g" ${pyproject_file}
