64302a6bf511428e6500c658fec7cc7c9ab78675
Peter Andreas Entschev <peter@entschev.com>
2024-02-29 14:15:55 +0100

    Fix test file key
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- ci/test_python_distributed.sh
+++ ci/test_python_distributed.sh
@@ -12,7 +12,7 @@ rapids-logger "Create test conda environment"
 
 rapids-dependency-file-generator \
   --output conda \
-  --file_key test_python \
+  --file_key test_python_distributed \
   --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
 
 rapids-mamba-retry env create --force -f env.yaml -n test
