d3210fcb8a5ffba4c388adfde4be9711030bc03e
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-29 18:32:01 +0000

    Also download ucx wheel for tests

--- ci/test_wheel_ucxx.sh
+++ ci/test_wheel_ucxx.sh
@@ -3,16 +3,17 @@
 
 set -euo pipefail
 
-PROJECT_NAME="ucxx"
+package_name="ucxx"
 
 source "$(dirname "$0")/test_common.sh"
 
 mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="${PROJECT_NAME}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install $(echo ./dist/${PROJECT_NAME}*.whl)[test]
+libucx_wheelhouse=$(RAPIDS_PY_WHEEL_NAME="${RAPIDS_PY_CUDA_SUFFIX}" rapids-get-pr-wheel-artifact ucx-wheels 1 cpp)
+
+python -m pip install "${package_name}-${RAPIDS_PY_CUDA_SUFFIX}[test]" --find-links dist/ --find-links "${libucx_wheelhouse}"
 
 rapids-logger "Python Core Tests"
 run_py_tests
