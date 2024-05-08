2195ceabf35b404b3ae6b09f784d1d312b4b6fce
Peter Andreas Entschev <peter@entschev.com>
2024-05-08 19:14:58 +0200

    Use `weak_ptr<ucxx::Endpoint>` in `ErrorCallbackData` (#227)
    A race could cause the `Endpoint::errorCallback` to dereference an already closed and destroyed `ucxx::Endpoint`, leading to segmentation faults and sometimes deadlocks. With the use of a `weak_ptr<Endpoint>` that can be prevented by returning before dereferencing the pointer which should be safe since the owner has already been destroyed anyway.
    
    Authors:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
    
    URL: https://github.com/rapidsai/ucxx/pull/227

--- cpp/src/endpoint.cpp
+++ cpp/src/endpoint.cpp
@@ -34,7 +34,7 @@
 
 namespace ucxx {
 
-ErrorCallbackData::ErrorCallbackData(Endpoint* endpoint,
+ErrorCallbackData::ErrorCallbackData(std::shared_ptr<Endpoint> endpoint,
                                      std::shared_ptr<InflightRequests> inflightRequests,
                                      std::shared_ptr<Worker> worker)
   : endpoint(endpoint), inflightRequests(inflightRequests), worker(worker)
@@ -55,9 +55,7 @@ static std::shared_ptr<Worker> getWorker(std::shared_ptr<Component> workerOrList
   return worker;
 }
 
-Endpoint::Endpoint(std::shared_ptr<Component> workerOrListener,
-                   ucp_ep_params_t* params,
-                   bool endpointErrorHandling)
+Endpoint::Endpoint(std::shared_ptr<Component> workerOrListener, bool endpointErrorHandling)
   : _endpointErrorHandling{endpointErrorHandling}
 {
   auto worker = ::ucxx::getWorker(workerOrListener);
@@ -66,21 +64,29 @@ Endpoint::Endpoint(std::shared_ptr<Component> workerOrListener,
     throw ucxx::Error("Worker not initialized");
 
   setParent(workerOrListener);
+}
 
-  _callbackData = std::make_unique<ErrorCallbackData>(this, _inflightRequests, worker);
+void Endpoint::create(ucp_ep_params_t* params)
+{
+  auto worker   = ::ucxx::getWorker(_parent);
+  _callbackData = std::make_unique<ErrorCallbackData>(
+    std::dynamic_pointer_cast<Endpoint>(shared_from_this()), _inflightRequests, worker);
 
-  params->err_mode =
-    (endpointErrorHandling ? UCP_ERR_HANDLING_MODE_PEER : UCP_ERR_HANDLING_MODE_NONE);
-  params->err_handler.cb  = Endpoint::errorCallback;
-  params->err_handler.arg = _callbackData.get();
+  if (_endpointErrorHandling) {
+    params->err_mode        = UCP_ERR_HANDLING_MODE_PEER;
+    params->err_handler.cb  = Endpoint::errorCallback;
+    params->err_handler.arg = _callbackData.get();
+  } else {
+    params->err_mode        = UCP_ERR_HANDLING_MODE_NONE;
+    params->err_handler.cb  = nullptr;
+    params->err_handler.arg = nullptr;
+  }
 
   if (worker->isProgressThreadRunning()) {
     ucs_status_t status = UCS_INPROGRESS;
     utils::CallbackNotifier callbackNotifier{};
-    auto worker = ::ucxx::getWorker(_parent);
-    worker->registerGenericPre([this, &params, &callbackNotifier, &status]() {
-      auto worker = ::ucxx::getWorker(_parent);
-      status      = ucp_ep_create(worker->getHandle(), params, &_handle);
+    worker->registerGenericPre([this, &worker, &params, &callbackNotifier, &status]() {
+      status = ucp_ep_create(worker->getHandle(), params, &_handle);
       callbackNotifier.set();
     });
 
@@ -100,7 +106,7 @@ Endpoint::Endpoint(std::shared_ptr<Component> workerOrListener,
              this,
              _handle,
              _parent.get(),
-             endpointErrorHandling);
+             _endpointErrorHandling);
 }
 
 std::shared_ptr<Endpoint> createEndpointFromHostname(std::shared_ptr<Worker> worker,
@@ -120,7 +126,9 @@ std::shared_ptr<Endpoint> createEndpointFromHostname(std::shared_ptr<Worker> wor
   params.sockaddr.addrlen = info->ai_addrlen;
   params.sockaddr.addr    = info->ai_addr;
 
-  return std::shared_ptr<Endpoint>(new Endpoint(worker, &params, endpointErrorHandling));
+  auto ep = std::shared_ptr<Endpoint>(new Endpoint(worker, endpointErrorHandling));
+  ep->create(&params);
+  return ep;
 }
 
 std::shared_ptr<Endpoint> createEndpointFromConnRequest(std::shared_ptr<Listener> listener,
@@ -136,7 +144,9 @@ std::shared_ptr<Endpoint> createEndpointFromConnRequest(std::shared_ptr<Listener
     .flags        = UCP_EP_PARAMS_FLAGS_NO_LOOPBACK,
     .conn_request = connRequest};
 
-  return std::shared_ptr<Endpoint>(new Endpoint(listener, &params, endpointErrorHandling));
+  auto ep = std::shared_ptr<Endpoint>(new Endpoint(listener, endpointErrorHandling));
+  ep->create(&params);
+  return ep;
 }
 
 std::shared_ptr<Endpoint> createEndpointFromWorkerAddress(std::shared_ptr<Worker> worker,
@@ -153,7 +163,9 @@ std::shared_ptr<Endpoint> createEndpointFromWorkerAddress(std::shared_ptr<Worker
                                           UCP_EP_PARAM_FIELD_ERR_HANDLER,
                             .address = address->getHandle()};
 
-  return std::shared_ptr<Endpoint>(new Endpoint(worker, &params, endpointErrorHandling));
+  auto ep = std::shared_ptr<Endpoint>(new Endpoint(worker, endpointErrorHandling));
+  ep->create(&params);
+  return ep;
 }
 
 Endpoint::~Endpoint()
@@ -566,36 +578,58 @@ std::shared_ptr<Worker> Endpoint::getWorker() { return ::ucxx::getWorker(_parent
 void Endpoint::errorCallback(void* arg, ucp_ep_h ep, ucs_status_t status)
 {
   ErrorCallbackData* data = reinterpret_cast<ErrorCallbackData*>(arg);
-  if (data->closing.exchange(true)) return;
-  data->status = status;
-  data->worker->scheduleRequestCancel(data->inflightRequests->release());
-  {
-    std::lock_guard<std::mutex> lock(data->mutex);
-    if (data->closeCallback) {
-      ucxx_debug("ucxx::Endpoint::%s: %p, UCP handle: %p, calling user close callback",
-                 __func__,
-                 data->endpoint,
-                 ep);
-      data->closeCallback(status, data->closeCallbackArg);
-      data->closeCallback    = nullptr;
-      data->closeCallbackArg = nullptr;
-    }
-  }
 
-  // Connection reset and timeout often represent just a normal remote
-  // endpoint disconnect, log only in debug mode.
-  if (status == UCS_ERR_CONNECTION_RESET || status == UCS_ERR_ENDPOINT_TIMEOUT)
-    ucxx_debug("ucxx::Endpoint::%s, UCP handle: %p, error callback called with status %d: %s",
-               __func__,
-               ep,
-               status,
-               ucs_status_string(status));
-  else
+  // Unable to cast to `ErrorCallbackData*`: invalid `arg`.
+  if (data == nullptr) {
     ucxx_error("ucxx::Endpoint::%s, UCP handle: %p, error callback called with status %d: %s",
                __func__,
                ep,
                status,
                ucs_status_string(status));
+    return;
+  }
+
+  try {
+    std::shared_ptr<Endpoint> endpoint(data->endpoint);
+
+    // Endpoint is already closing.
+    if (data->closing.exchange(true)) return;
+
+    data->status = status;
+    data->worker->scheduleRequestCancel(data->inflightRequests->release());
+    {
+      std::lock_guard<std::mutex> lock(data->mutex);
+      if (data->closeCallback) {
+        ucxx_debug("ucxx::Endpoint::%s: %p, UCP handle: %p, calling user close callback",
+                   __func__,
+                   endpoint.get(),
+                   ep);
+        data->closeCallback(status, data->closeCallbackArg);
+        data->closeCallback    = nullptr;
+        data->closeCallbackArg = nullptr;
+      }
+    }
+
+    // Connection reset and timeout often represent just a normal remote
+    // endpoint disconnect, log only in debug mode.
+    if (status == UCS_ERR_CONNECTION_RESET || status == UCS_ERR_ENDPOINT_TIMEOUT)
+      ucxx_debug("ucxx::Endpoint::%s: %p, UCP handle: %p, error callback called with status %d: %s",
+                 __func__,
+                 endpoint.get(),
+                 ep,
+                 status,
+                 ucs_status_string(status));
+    else
+      ucxx_error("ucxx::Endpoint::%s: %p, UCP handle: %p, error callback called with status %d: %s",
+                 __func__,
+                 endpoint.get(),
+                 ep,
+                 status,
+                 ucs_status_string(status));
+  } catch (std::bad_weak_ptr& exception) {
+    // Unable to acquire `std::shared_ptr<ucxx::Endpoint>`: owner was already destroyed.
+    return;
+  }
 }
 
 }  // namespace ucxx
