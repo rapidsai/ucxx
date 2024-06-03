e5c54d71641d6714e98a07fbefa639678ff7a06e
James Lamb <jlamb@nvidia.com>
2024-06-03 15:09:12 -0700

    add test, remove unnecessary copyright

--- python/ucxx/_version.py
+++ python/ucxx/_version.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright (c) 2024, NVIDIA CORPORATION.
-
 import importlib.resources
 
 __version__ = (
