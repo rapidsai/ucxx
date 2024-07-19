df44fe2735ff3395d2530bc3def0deadd7693ed4
Vyas Ramasubramani <vyasr@nvidia.com>
2024-07-19 00:43:24 +0000

    Document rattler-build options

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -17,11 +17,22 @@ rapids-print-env
 
 rapids-logger "Begin C++ and Python builds"
 
-#rapids-conda-retry mambabuild \
-#  conda/recipes/ucxx
+# TODO: Upstream this to the image.
+mamba install rattler-build -c conda-forge
 
-#mamba install rattler-build -c conda-forge
-
-RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rattler-build build --recipe conda/recipes/ucxx/meta.yaml --variant-config conda/recipes/ucxx/conda_build_config.yaml -c rapidsai-nightly -c conda-forge --output-dir /tmp/output/ --experimental --no-build-id
+RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rattler-build build \
+    # rattler-build uses recipe.yaml, not meta.yaml
+    --recipe conda/recipes/ucxx/meta.yaml \
+    # rattler-build uses variants.yaml, not conda_build_config.yaml
+    --variant-config conda/recipes/ucxx/conda_build_config.yaml \
+    # rattler-build does not respect .condarc, so channels and the output dir
+    # must be explicitly specified
+    -c rapidsai-nightly -c conda-forge \
+    --output-dir ${RAPIDS_CONDA_BLD_OUTPUT_DIR}
+    # The multi-output cache is currently an experimental feature.
+    # (https://prefix-dev.github.io/rattler-build/dev/multiple_output_cache/)
+    --experimental
+    # By default rattler-build adds a timestamp that defeats sccache caching.
+    --no-build-id
 
 rapids-upload-conda-to-s3 cpp
