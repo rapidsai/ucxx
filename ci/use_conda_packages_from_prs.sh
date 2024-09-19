aefc3a4a9dd644d0e930286d56321f6caaf9f503
James Lamb <jlamb@nvidia.com>
2024-09-18 19:53:01 -0500

    typo

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -6,6 +6,6 @@ CUDF_CPP_CHANNEL=$(rapids-get-pr-conda-artifact cudf 16806 cpp)
 CUDF_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact cudf 16806 python)
 
 conda config --system --add channels "${LIBRMM_CHANNEL}"
-conda config --system --add channels "${MM_CHANNEL}"
+conda config --system --add channels "${RMM_CHANNEL}"
 conda config --system --add channels "${CUDF_CPP_CHANNEL}"
 conda config --system --add channels "${CUDF_PYTHON_CHANNEL}"
