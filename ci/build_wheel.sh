bc3fe6023a4d31262a3f3b60cf5313c548822a36
James Lamb <jlamb@nvidia.com>
2024-10-16 12:00:42 -0500

    build without build isolation

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 package_name=$1
 package_dir=$2
 package_type=$3
+underscore_package_name=$(echo "${package_name}" | tr "-" "_")
 
 # The list of shared libraries to exclude from wheels varies by project.
 #
@@ -32,6 +33,27 @@ source rapids-configure-sccache
 source rapids-date-string
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+
+rapids-logger "Generating build requirements"
+matrix_selectors="cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION};cuda_suffixed=true"
+
+rapids-dependency-file-generator \
+  --output requirements \
+  --file-key "py_build_${underscore_package_name}" \
+  --matrix "${matrix_selectors}" \
+| tee /tmp/requirements-build.txt
+
+rapids-dependency-file-generator \
+  --output requirements \
+  --file-key "py_rapids_build_${underscore_package_name}" \
+  --matrix "${matrix_selectors}" \
+| tee -a /tmp/requirements-build.txt
+
+rapids-logger "Installing build requirements"
+python -m pip install \
+    -v \
+    --prefer-binary \
+    -r /tmp/requirements-build.txt
 
 rapids-generate-version > ./VERSION
 
