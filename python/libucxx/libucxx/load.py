29282423bf30a9780cb4f4a3ea70085cb0bd1cd8
James Lamb <jaylamb20@gmail.com>
2024-11-15 11:54:02 -0600

    Update python/libucxx/libucxx/load.py

--- python/libucxx/libucxx/load.py
+++ python/libucxx/libucxx/load.py
@@ -85,5 +85,5 @@ def load_library():
 
     # The caller almost never needs to do anything with this library, but no
     # harm in offering the option since this object at least provides a handle
-    # to inspect where the libucxx was loaded from.
+    # to inspect where libucxx was loaded from.
     return libucxx_lib
