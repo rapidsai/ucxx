8ae39886821815718adfd3473e0e61341201e4c4
James Lamb <jlamb@nvidia.com>
2025-01-24 14:50:36 -0600

    revert style stuff

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -16,11 +16,5 @@ rapids-dependency-file-generator \
 rapids-mamba-retry env create --yes -f env.yaml -n checks
 conda activate checks
 
-# get config for cmake-format checks
-FORMAT_FILE_URL="https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-25.02/cmake-format-rapids-cmake.json"
-export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
-mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
-wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
-
 # Run pre-commit checks
 pre-commit run --all-files --show-diff-on-failure
