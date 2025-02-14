03bcace5d2fb8c3516e590b373ff9aa90a900f5d
Kyle Edwards <kyedwards@nvidia.com>
2025-02-14 14:34:48 -0500

    Consolidate more Conda solves in CI
    Issue: https://github.com/rapidsai/build-planning/issues/22

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2025, NVIDIA CORPORATION.
 
 set -euo pipefail
+
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
 rapids-logger "Create test conda environment"
 . /opt/conda/etc/profile.d/conda.sh
 
-export UCXX_VERSION="$(head -1 ./VERSION)"
 UCXX_VERSION_MAJOR_MINOR="$(sed -E -e 's/^([0-9]+)\.([0-9]+)\.([0-9]+).*$/\1.\2/' VERSION)"
 
 ENV_YAML_DIR="$(mktemp -d)"
@@ -14,19 +16,14 @@ ENV_YAML_DIR="$(mktemp -d)"
 rapids-dependency-file-generator \
   --output conda \
   --file-key docs \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee "${ENV_YAML_DIR}/env.yaml"
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" \
+  --prepend-channel "${CPP_CHANNEL}" \
+  | tee "${ENV_YAML_DIR}/env.yaml"
 
 rapids-mamba-retry env create --yes -f "${ENV_YAML_DIR}/env.yaml" -n docs
 conda activate docs
 
 rapids-print-env
-
-rapids-logger "Downloading artifacts from previous jobs"
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-
-rapids-mamba-retry install \
-  --channel "${CPP_CHANNEL}" \
-  "libucxx=${UCXX_VERSION}"
 
 export RAPIDS_DOCS_DIR="$(mktemp -d)"
 
