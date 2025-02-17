960fab8212725f70ff62d476029a6dcf2e761c54
Peter Andreas Entschev <peter@entschev.com>
2025-02-17 05:01:45 -0800

    Remove temporary files

--- test_cython.py
+++
@@ -1,7 +0,0 @@
-from ucxx._lib.tests_cython.test_cython import *
-# import ucxx._lib.libucxx as ucx_api
-
-
-def test_context_getter():
-    # assert ucx_api is not None
-    return cython_test_context_getter()
