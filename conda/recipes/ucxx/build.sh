0fe315a18eb76a3e70a2f3fbe0f71d581947504e
Gil Forsyth <gforsyth@nvidia.com>
2025-02-20 13:02:33 -0500

    inline build.sh

--- conda/recipes/ucxx/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.
-# SPDX-License-Identifier: BSD-3-Clause
-
-export ucxx_ROOT="$(realpath ./cpp/build)"
-./build.sh -n -v libucxx libucxx_python benchmarks examples tests --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
