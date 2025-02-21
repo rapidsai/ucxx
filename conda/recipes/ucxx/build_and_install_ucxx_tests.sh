fd917fbf74386eafdb2355c5e19afe945c57c48c
Peter Andreas Entschev <peter@entschev.com>
2025-02-21 06:09:04 -0800

    Fix conda ucxx-tests build script

--- conda/recipes/ucxx/build_and_install_ucxx_tests.sh
+++ conda/recipes/ucxx/build_and_install_ucxx_tests.sh
@@ -3,5 +3,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: BSD-3-Clause
 
-cmake --install cpp/python/build --component testing
-./build.sh ucxx_tests --cmake-args="\"-DFIND_UCXX_PYTHON=ON -Ducxx-python_DIR=$(realpath ./cpp/python/build)\""
+cmake --install cpp/python/build
+./build.sh ucxx ucxx_tests --cmake-args="\"-DFIND_UCXX_PYTHON=ON -Ducxx-python_DIR=$(realpath ./cpp/python/build)\""
