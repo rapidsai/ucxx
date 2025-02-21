85157fbb5d8c1bb91dc50858621abc60b6a6a5a4
Peter Andreas Entschev <peter@entschev.com>
2025-02-21 08:56:56 -0800

    Fix Cython module import in test_cython

--- python/ucxx/ucxx/_lib/tests/test_cython.py
+++ python/ucxx/ucxx/_lib/tests/test_cython.py
@@ -3,9 +3,9 @@
 
 import pytest
 
-from ucxx._lib.tests_cython.test_cython import cython_test_context_getter
-
 
 @pytest.mark.cython
 def test_context_getter():
+    from ucxx._lib.tests_cython.test_cython import cython_test_context_getter
+
     return cython_test_context_getter()
