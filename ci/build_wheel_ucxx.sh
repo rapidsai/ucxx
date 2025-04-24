402eece0dde89278e85a60ab22e3e5decf0cb775
Jaya Venkatesh <jjayabaskar@nvidia.com>
2025-04-24 11:33:18 -0700

    Download build artifacts from Github for CI (#399)
    This work is towards moving build artifacts from `downloads.rapids.ai` to Github Artifact Store (see https://github.com/rapidsai/ops/issues/2982)
    
    Updates conda and wheel artifact download source from S3 to GitHub across CI scripts, wherever applicable. 
    
    Uses dynamic temporary paths for wheel downloads returned by `rapids-download-wheels-from-github` instead of using fixed directories, to streamline wheel downloads in the same way as conda downloads.
    
    Also updates CI workflows to follow `package-name` convention between wheel build and wheel publish jobs.
    
    Authors:
      - Jaya Venkatesh (https://github.com/VenkateshJaya)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/ucxx/pull/399

--- ci/build_wheel_ucxx.sh
+++ ci/build_wheel_ucxx.sh
@@ -13,8 +13,8 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 #
 # Using env variable PIP_CONSTRAINT is necessary to ensure the constraints
 # are used when creating the isolated build environment.
-RAPIDS_PY_WHEEL_NAME="libucxx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/libucxx_dist
-echo "libucxx-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo /tmp/libucxx_dist/libucxx_*.whl)" > /tmp/constraints.txt
+LIBUCXX_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libucxx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
+echo "libucxx-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBUCXX_WHEELHOUSE}"/libucxx_*.whl)" > /tmp/constraints.txt
 export PIP_CONSTRAINT="/tmp/constraints.txt"
 
 export SKBUILD_CMAKE_ARGS="-DFIND_UCXX_CPP=ON;-DCMAKE_INSTALL_LIBDIR=ucxx/lib64;-DCMAKE_INSTALL_INCLUDEDIR=ucxx/include"
