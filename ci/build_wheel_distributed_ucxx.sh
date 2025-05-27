b4758ca473e01c9e3bec52b2863529ddf39f9bfd
James Lamb <jlamb@nvidia.com>
2025-05-27 14:08:46 -0500

    stop uploading packages to downloads.rapids.ai

--- ci/build_wheel_distributed_ucxx.sh
+++ ci/build_wheel_distributed_ucxx.sh
@@ -7,10 +7,6 @@ source rapids-init-pip
 
 package_dir="python/distributed-ucxx"
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
-
 ./ci/build_wheel.sh distributed-ucxx "${package_dir}"
 cp "${package_dir}/dist"/* "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}/"
 ./ci/validate_wheel.sh "${package_dir}" "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
-
-RAPIDS_PY_WHEEL_NAME="distributed_ucxx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
