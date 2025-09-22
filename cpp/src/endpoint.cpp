9eedc3d992652f967522a6c01e41a96282d6579a
Peter Andreas Entschev <peter@entschev.com>
2025-09-22 21:40:10 +0200

    Make send/put buffer pointers const (#516)
    To prevent unintentional modification of send/put buffers and also to make it clear for users that those methods are not supposed modify data/pointers, mark them `const`.
    
    In `tagMultiSend` only the data can be `const`, the pointers themselves need to be modifiable by `std::vector`. Perhaps it would make sense to have a specialized container for `tagMultiSend` in the future.
    
    Authors:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    Approvers:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    URL: https://github.com/rapidsai/ucxx/pull/516

--- cpp/src/endpoint.cpp
+++ cpp/src/endpoint.cpp
@@ -442,7 +442,7 @@ size_t Endpoint::cancelInflightRequestsBlocking(uint64_t period, uint64_t maxAtt
 size_t Endpoint::getCancelingSize() const { return _inflightRequests->getCancelingSize(); }
 
 std::shared_ptr<Request> Endpoint::amSend(
-  void* buffer,
+  const void* const buffer,
   const size_t length,
   const ucs_memory_type_t memoryType,
   const std::optional<AmReceiverCallbackInfo> receiverCallbackInfo,
@@ -502,7 +502,7 @@ std::shared_ptr<Request> Endpoint::memGet(void* buffer,
     callbackData));
 }
 
-std::shared_ptr<Request> Endpoint::memPut(void* buffer,
+std::shared_ptr<Request> Endpoint::memPut(const void* const buffer,
                                           size_t length,
                                           uint64_t remoteAddr,
                                           ucp_rkey_h rkey,
@@ -518,7 +518,7 @@ std::shared_ptr<Request> Endpoint::memPut(void* buffer,
                                                   callbackData));
 }
 
-std::shared_ptr<Request> Endpoint::memPut(void* buffer,
+std::shared_ptr<Request> Endpoint::memPut(const void* const buffer,
                                           size_t length,
                                           std::shared_ptr<RemoteKey> remoteKey,
                                           uint64_t remoteAddressOffset,
@@ -536,7 +536,7 @@ std::shared_ptr<Request> Endpoint::memPut(void* buffer,
     callbackData));
 }
 
-std::shared_ptr<Request> Endpoint::streamSend(void* buffer,
+std::shared_ptr<Request> Endpoint::streamSend(const void* const buffer,
                                               size_t length,
                                               const bool enablePythonFuture)
 {
@@ -554,7 +554,7 @@ std::shared_ptr<Request> Endpoint::streamRecv(void* buffer,
     createRequestStream(endpoint, data::StreamReceive(buffer, length), enablePythonFuture));
 }
 
-std::shared_ptr<Request> Endpoint::tagSend(void* buffer,
+std::shared_ptr<Request> Endpoint::tagSend(const void* const buffer,
                                            size_t length,
                                            Tag tag,
                                            const bool enablePythonFuture,
@@ -585,7 +585,7 @@ std::shared_ptr<Request> Endpoint::tagRecv(void* buffer,
                                                   callbackData));
 }
 
-std::shared_ptr<Request> Endpoint::tagMultiSend(const std::vector<void*>& buffer,
+std::shared_ptr<Request> Endpoint::tagMultiSend(const std::vector<const void*>& buffer,
                                                 const std::vector<size_t>& size,
                                                 const std::vector<int>& isCUDA,
                                                 const Tag tag,
--- cpp/src/request_data.cpp
+++ cpp/src/request_data.cpp
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES.
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES.
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #include <stdexcept>
@@ -15,7 +15,7 @@ namespace ucxx {
 
 namespace data {
 
-AmSend::AmSend(const void* buffer,
+AmSend::AmSend(const void* const buffer,
                const size_t length,
                const ucs_memory_type memoryType,
                const std::optional<AmReceiverCallbackInfo> receiverCallbackInfo)
@@ -32,7 +32,7 @@ EndpointClose::EndpointClose(const bool force) : _force(force) {}
 
 Flush::Flush() {}
 
-MemPut::MemPut(const void* buffer,
+MemPut::MemPut(const void* const buffer,
                const size_t length,
                const uint64_t remoteAddr,
                const ucp_rkey_h rkey)
@@ -45,7 +45,8 @@ MemGet::MemGet(void* buffer, const size_t length, const uint64_t remoteAddr, con
 {
 }
 
-StreamSend::StreamSend(const void* buffer, const size_t length) : _buffer(buffer), _length(length)
+StreamSend::StreamSend(const void* const buffer, const size_t length)
+  : _buffer(buffer), _length(length)
 {
   /**
    * Stream API does not support zero-sized messages. See
@@ -65,7 +66,7 @@ StreamReceive::StreamReceive(void* buffer, const size_t length) : _buffer(buffer
   if (length == 0) throw std::runtime_error("Length has to be a positive value.");
 }
 
-TagSend::TagSend(const void* buffer, const size_t length, const ::ucxx::Tag tag)
+TagSend::TagSend(const void* const buffer, const size_t length, const ::ucxx::Tag tag)
   : _buffer(buffer), _length(length), _tag(tag)
 {
 }
@@ -78,7 +79,7 @@ TagReceive::TagReceive(void* buffer,
 {
 }
 
-TagMultiSend::TagMultiSend(const std::vector<void*>& buffer,
+TagMultiSend::TagMultiSend(const std::vector<const void*>& buffer,
                            const std::vector<size_t>& length,
                            const std::vector<int>& isCUDA,
                            const ::ucxx::Tag tag)
