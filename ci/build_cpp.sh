98d0bc9ffa9fce0b8434ed96a089b42e86c499b8
ptaylor <paul.e.taylor@me.com>
2025-11-19 19:04:00 +0000

    use S3 as the preprocessor cache location

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -30,6 +30,7 @@ rattler-build build --recipe conda/recipes/ucxx \
                     "${RATTLER_CHANNELS[@]}"
 
 sccache --show-adv-stats
+sccache --stop-server >/dev/null 2>&1 || true
 
 # remove build_cache directory
 rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
