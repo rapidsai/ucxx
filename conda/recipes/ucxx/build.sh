4e170e0bd2b285ef635c3ae9a7b9aa065971f2a9
ptaylor <paul.e.taylor@me.com>
2025-11-20 12:01:23 -0800

    Revert "set SCCACHE_RECACHE=1"
    This reverts commit ed0e83590097605b443f83e3f3cd8c04ccd9a49b.

--- conda/recipes/ucxx/build.sh
+++ conda/recipes/ucxx/build.sh
@@ -3,7 +3,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: BSD-3-Clause
 
-export SCCACHE_RECACHE=1
 ucxx_ROOT="$(realpath ./cpp/build)"
 export ucxx_ROOT
 ./build.sh -n -v libucxx libucxx_python libucxx_benchmarks libucxx_examples libucxx_tests --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
