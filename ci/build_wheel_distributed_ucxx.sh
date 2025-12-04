5721c6ced7fddf7dbe58d4e157f7997b0fe4db71
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-12-04 09:41:01 -0500

    refactor(build): build pure Python wheel for `distributed-ucxx` (#556)
    - **refactor(distributed-ucxx): build as pure-python wheel**
    - **refactor(conda): relax Python version in `distributed-ucxx` conda package**
    
    xref rapidsai/build-planning#43
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/ucxx/pull/556

--- ci/build_wheel_distributed_ucxx.sh
+++ ci/build_wheel_distributed_ucxx.sh
@@ -11,3 +11,6 @@ package_dir="python/distributed-ucxx"
 ./ci/build_wheel.sh distributed-ucxx "${package_dir}"
 cp "${package_dir}/dist"/* "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}/"
 ./ci/validate_wheel.sh "${package_dir}" "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python distributed-ucxx --pure --cuda "${RAPIDS_CUDA_VERSION}")"
+export RAPIDS_PACKAGE_NAME
