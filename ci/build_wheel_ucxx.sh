999495bb9183ceed4518efd5ff4c3d0385a6d366
Gil Forsyth <gforsyth@nvidia.com>
2026-01-05 12:53:11 -0500

    feat: build python packages using the limited API

--- ci/build_wheel_ucxx.sh
+++ ci/build_wheel_ucxx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: BSD-3-Clause
 
 set -euo pipefail
@@ -32,3 +32,9 @@ python -m auditwheel repair \
     ${package_dir}/dist/*
 
 ./ci/validate_wheel.sh "${package_dir}" "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+# Only use stable ABI package naming for Python >= 3.11
+if [[ "${RAPIDS_PY_VERSION}" != "3.10" ]]; then
+  RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python ucxx --stable --cuda)"
+  export RAPIDS_PACKAGE_NAME
+fi
