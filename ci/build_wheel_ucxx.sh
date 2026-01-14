174698db29eb391a7508266f664218aeca181565
Mike McCarty <mmccarty@nvidia.com>
2026-01-14 11:41:29 -0500

    reverted copy right headers in ci scripts

--- ci/build_wheel_ucxx.sh
+++ ci/build_wheel_ucxx.sh
@@ -14,7 +14,7 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 # Downloads libucxx wheel from this current build,
 # then ensures 'ucxx' wheel builds always use the 'libucxx' just built in the same CI run.
 #
-# env variable 'PIP_CONSTRAINT' is set up by rapids-init-pip. It constrains all subsequent 
+# env variable 'PIP_CONSTRAINT' is set up by rapids-init-pip. It constrains all subsequent
 # 'pip install', 'pip download', etc. calls (except those used in 'pip wheel', handled separately in build scripts)
 LIBUCXX_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libucxx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 echo "libucxx-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBUCXX_WHEELHOUSE}"/libucxx_*.whl)" >> "${PIP_CONSTRAINT}"
