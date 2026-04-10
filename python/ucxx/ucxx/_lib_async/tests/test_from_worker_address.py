a83edbdf62112bc97924e4d12818dc8126c9e2ca
jakirkham <jakirkham@gmail.com>
2026-04-09 17:59:24 -0700

    Use header format to add address string

--- python/ucxx/ucxx/_lib_async/tests/test_from_worker_address.py
+++ python/ucxx/ucxx/_lib_async/tests/test_from_worker_address.py
@@ -131,7 +131,7 @@ def _pack_address_and_tag(address, recv_tag, send_tag):
 
     fixed_size_address_packed = np.empty(address_info["frame_size"], dtype=np.uint8)
     struct.pack_into(
-        address_info["fixed_size_address_buffer_fmt"],
+        f"""{address_info["header_fmt"]}{address.length}s""",
         fixed_size_address_packed,  # Buffer to fill
         0,  # Starting Offset
         recv_tag,  # Recv Tag
