a39657d2393920f04044482098f4ae097a99ea08
jakirkham <jakirkham@gmail.com>
2026-04-09 18:38:36 -0700

    Put buffer and offset on separate lines

--- python/ucxx/ucxx/_lib_async/tests/test_from_worker_address.py
+++ python/ucxx/ucxx/_lib_async/tests/test_from_worker_address.py
@@ -129,7 +129,8 @@ def _pack_address_and_tag(address, recv_tag, send_tag):
     fixed_size_address_packed = np.empty(address_info["frame_size"], dtype=np.uint8)
     struct.pack_into(
         address_info["fixed_size_address_buffer_fmt"],
-        fixed_size_address_packed, 0,  # Buffer to fill & Starting Offset
+        fixed_size_address_packed,     # Buffer to fill
+        0,                             # Starting Offset
         recv_tag,                      # Recv Tag
         send_tag,                      # Send Tag
         address.length,                # Address buffer length
