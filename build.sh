f4a5e39d7e62c61c16ea55d4f6826c584ecb5a1d
Kyle Edwards <kyedwards@nvidia.com>
2026-09-16 16:14:33 -0400

    Use new `verify-dependencies` hook from pre-commit-hooks (#762)
    This hook enforces `dependencies.yaml` conventions, starting with ensuring that the correct `cuda_suffixed` and `use_cuda_wheels` matrix entries are used.
    
    Issue: https://github.com/rapidsai/pre-commit-hooks/issues/132
    
    Authors:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/ucxx/pull/762

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
 # UCXX build script
@@ -46,6 +46,12 @@ PYTHON_BUILD_DIR=${PYTHON_BUILD_DIR:=${REPODIR}/cpp/python/build}
 UCXX_BUILD_DIR=${REPODIR}/python/ucxx/build
 
 BUILD_DIRS="${LIB_BUILD_DIR} ${PYTHON_BUILD_DIR} ${UCXX_BUILD_DIR}"
+
+CUDA_VERSION="${RAPIDS_CUDA_VERSION:-$(nvcc --version | sed -E -n "s/^.*release ([0-9]+\.[0-9]+).*$/\1/p")}"
+if [[ -z "$CUDA_VERSION" ]]; then
+    echo "Could not determine CUDA version. Please set RAPIDS_CUDA_VERSION or make sure your \$PATH contains a valid nvcc."
+    exit 1
+fi
 
 # Set defaults for vars modified by flags to this script
 VERBOSE_FLAG=""
@@ -276,6 +282,7 @@ if buildAll || hasArg ucxx; then
             --no-build-isolation \
             --no-deps \
             --config-settings rapidsai.disable-cuda=true \
+            --config-settings "rapidsai.matrix-entry=cuda=${CUDA_VERSION};cuda_suffixed=false;use_cuda_wheels=false" \
             --config-settings skbuild.install.components=ucxx \
             --config-settings skbuild.install.components=examples \
             "${PY_API_ARGS[@]}" \
@@ -289,5 +296,5 @@ fi
 if buildAll || hasArg distributed_ucxx; then
 
     cd "${REPODIR}/python/distributed-ucxx/"
-    python -m pip install --no-build-isolation --no-deps --config-settings rapidsai.disable-cuda=true .
+    python -m pip install --no-build-isolation --no-deps --config-settings rapidsai.disable-cuda=true --config-settings "rapidsai.matrix-entry=cuda=${CUDA_VERSION};cuda_suffixed=false;use_cuda_wheels=false" .
 fi
