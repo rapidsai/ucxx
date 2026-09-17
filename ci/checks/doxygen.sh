35e56f3f8847da97e0a03c4bd510a26e6ad6aa5f
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2026-09-17 11:44:00 -0700

    Update Doxygen to 1.18.0 (#761)
    This PR updates Doxygen to 1.18.0 and refreshes generated dependency files.
    
    The Doxyfile update removes options that are obsolete in Doxygen 1.18 and preserves GitHub-compatible Markdown heading identifiers. Additional repository-specific documentation warning fixes may be required.
    
    Authors:
      - Vukasin Milovanovic (https://github.com/vuule)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/ucxx/pull/761

--- ci/checks/doxygen.sh
+++ ci/checks/doxygen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 ###############################
 # UCXX doxygen warnings check #
@@ -8,11 +8,11 @@
 # Utility to return version as number for comparison
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
-# doxygen supported version 1.9.1
+# doxygen supported version 1.18.0
 DOXYGEN_VERSION=$(doxygen --version)
-if [ ! "$(version "$DOXYGEN_VERSION")" -eq "$(version "1.9.1")" ] ; then
+if [ ! "$(version "$DOXYGEN_VERSION")" -eq "$(version "1.18.0")" ] ; then
   echo -e "warning: Unsupported doxygen version $DOXYGEN_VERSION"
-  echo -e "Expecting doxygen version 1.9.1"
+  echo -e "Expecting doxygen version 1.18.0"
   exit 0
 fi
 
